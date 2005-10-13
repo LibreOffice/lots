500e38d8faef30c6b6c017bbf00680888a6f22fe
lut <devnull@localhost>
2005-10-13 18:54:05 +0200

    [svn r185] (LUT) +VisibleTextFragmentList.java       +Logger.java

---
+++ src/de/muenchen/allg/itd51/wollmux/Logger.java
@@ -0,0 +1,225 @@
+/*
+ * Dateiname: Logger.java
+ * Projekt  : WollMux
+ * Funktion : Logging-Mechanismus zum Schreiben von Nachrichten auf eine PrintStream.
+ * 
+ * Copyright: Landeshauptstadt München
+ *
+ * Änderungshistorie:
+ * Datum      | Wer | Änderungsgrund
+ * -------------------------------------------------------------------
+ * 13.10.2005 | LUT | Erstellung
+ * -------------------------------------------------------------------
+ *
+ * @author Christoph Lutz (D-III-ITD 5.1)
+ * @version 1.0
+ */
+
+package de.muenchen.allg.itd51.wollmux;
+
+import java.io.PrintStream;
+
+/**
+ * <p>
+ * Der Logger ist ein simpler Logging Mechanismus, der im Programmablauf
+ * auftretende Nachrichten verschiedener Prioritäten entgegennimmt und die
+ * Nachrichten entsprechend der Einstellung des Logging-Modus auf einem
+ * PrintStream ausgibt. Die Logging-Nachrichten werden über unterschiedliche
+ * Methodenaufrufe entsprechend der Logging-Priorität abgesetzt. Folgende
+ * Methoden stehen dafür zur Verfügung: critical(), log(), debug(), debug2()
+ * </p>
+ * <p>
+ * Der Logging-Modus kann über die init()-Methode initialisiert werden. Er
+ * beschreibt, welche Nachrichten aus den Prioritätsstufen angezeigt werden und
+ * welche nicht. Jeder Logging Modus zeigt die Nachrichten seiner Priorität und
+ * die Nachrichten der höheren Prioritätsstufen an. Standardmässig ist der Modus
+ * Logging.NONE voreingestellt.
+ * </p>
+ */
+public class Logger {
+
+	/**
+	 * Der PrintStream, auf den die Nachrichten geschrieben werden.
+	 */
+	private static PrintStream out;
+
+	/**
+	 * Im Logging-Modus <code>NONE</code> werden keine Nachrichten ausgegeben.
+	 * Dieser Modus ist die Defaulteinstellung.
+	 */
+	public static final int NONE = 0;
+
+	/**
+	 * Der Logging-Modus <code>CRITICAL</code> zeigt nur Nachrichten der
+	 * höchsten Prioritätsstufe an. CRITICAL enthält nur Nachrichten, die für
+	 * den Programmablauf kritisch sind - z.B. Fehlermeldungen und Exceptions.
+	 */
+	public static final int CRITICAL = 1;
+
+	/**
+	 * Der Logging-Modus <code>LOG</code> ist der Standard Modus. Er zeigt
+	 * Nachrichten und wichtige Programminformationen an, die im täglichen
+	 * Einsatz interessant sind.
+	 */
+	public static final int LOG = 3;
+
+	/**
+	 * Der Logging-Modus <code>DEBUG</code> wird genutzt, um detaillierte
+	 * Informationen über den Programmablauf auszugeben. Er ist vor allem für
+	 * DEBUG-Zwecke geeignet.
+	 */
+	public static final int DEBUG = 5;
+
+	/**
+	 * Der Logging-Modus <code>ALL</code> gibt uneingeschränkt alle
+	 * Nachrichten aus. Er enthält auch Nachrichten der Priorität debug2, die
+	 * sehr detaillierte Informationen ausgibt, die selbst für normale
+	 * DEBUG-Zwecke zu genau sind.
+	 */
+	public static final int ALL = 7;
+
+	/**
+	 * Das Feld <code>mode</code> enthält den aktuellen Logging-Mode
+	 */
+	private static int mode = LOG;
+
+	/**
+	 * Über die Methode init wird der Logger mit einem PrintStream und einem
+	 * Logging-Modus initialisiert. Ohne diese Methode verhält sich der Logger
+	 * stumm und erzeugt keinerlei Meldungen.
+	 * 
+	 * @param loggingMode
+	 *            Der neue Logging-Modus kann über die statischen Felder
+	 *            Logger.MODUS (z. B. Logger.DEBUG) angegeben werden.
+	 */
+	public static void init(PrintStream outputPrintStream, int loggingMode) {
+		out = outputPrintStream;
+		mode = loggingMode;
+		Logger.debug2("Logger::init(): LoggingMode = " + mode);
+	}
+
+	/**
+	 * Nachricht der höchsten Priorität "critical" absetzen. Als "critical" sind
+	 * nur Ereignisse einzustufen, die den Programmablauf unvorhergesehen
+	 * verändern.
+	 * 
+	 * @param msg
+	 *            Die Logging-Nachricht
+	 */
+	public static void critical(String msg) {
+		if (mode >= CRITICAL)
+			println("CRITICAL: " + msg);
+	}
+
+	/**
+	 * Wie {@link #critical(String)}, nur dass statt dem String eine Exception
+	 * ausgegeben wird.
+	 * 
+	 * @param e
+	 */
+	public static void critical(Exception e) {
+		if (mode >= CRITICAL)
+			printException("CRITICAL: ", e);
+	}
+
+	/**
+	 * Nachricht der Priorität "log" absetzen. "log" enthält alle Nachrichten,
+	 * die für den täglichen Programmablauf beim Endanwender oder zur Auffindung
+	 * der gängigsten Bedienfehler interessant sind.
+	 * 
+	 * @param msg
+	 *            Die Logging-Nachricht
+	 */
+	public static void log(String msg) {
+		if (mode >= LOG)
+			println("LOG: " + msg);
+	}
+
+	/**
+	 * Wie {@link #log(String)}, nur dass statt dem String eine Exception
+	 * ausgegeben wird.
+	 * 
+	 * @param e
+	 */
+	public static void log(Exception e) {
+		if (mode >= LOG)
+			printException("LOG: ", e);
+	}
+
+	/**
+	 * Nachricht der Priorität "debug" absetzen. Die debug-Priorität dient zu
+	 * debugging Zwecken. Sie enthält Informationen, die für Programmentwickler
+	 * interessant sind.
+	 * 
+	 * @param msg
+	 *            Die Logging-Nachricht
+	 */
+	public static void debug(String msg) {
+		if (mode >= DEBUG)
+			println("DEBUG: " + msg);
+	}
+
+	/**
+	 * Wie {@link #debug(String)}, nur dass statt dem String eine Exception
+	 * ausgegeben wird.
+	 * 
+	 * @param e
+	 */
+	public static void debug(Exception e) {
+		if (mode >= DEBUG)
+			printException("DEBUG: ", e);
+	}
+
+	/**
+	 * Nachricht der geringsten Priorität "debug2" absetzen. Das sind Meldungen,
+	 * die im Normalfall selbst für debugging-Zwecke zu detailliert sind.
+	 * Beispielsweise Logging-Meldungen von privaten Unterfunktionen, die die
+	 * Ausgabe nur unnötig unübersichtlich machen, aber nicht zum schnellen
+	 * Auffinden von Standard-Fehlern geeignet sind. "debug2" ist geeignet, um
+	 * ganz spezielle Fehler ausfindig zu machen.
+	 * 
+	 * @param msg
+	 *            Die Logging-Nachricht.
+	 */
+	public static void debug2(String msg) {
+		if (mode >= ALL)
+			println("DEBUG2: " + msg);
+	}
+
+	/**
+	 * Wie {@link #debug2(String)}, nur dass statt dem String eine Exception
+	 * ausgegeben wird.
+	 * 
+	 * @param e
+	 */
+	public static void debug2(Exception e) {
+		if (mode >= ALL)
+			printException("DEBUG2: ", e);
+	}
+
+	/**
+	 * Gebe den String s auf dem PrintStream aus.
+	 * 
+	 * @param s
+	 */
+	private static void println(String s) {
+		if (out != null) {
+			out.println(s);
+		}
+	}
+
+	/**
+	 * Gebe die Exception e samt StackTrace auf dem PrintStream aus.
+	 * 
+	 * @param s
+	 */
+	private static void printException(String prefix, Exception e) {
+		if (out != null) {
+			out.println(prefix + e.toString());
+			StackTraceElement[] se = e.getStackTrace();
+			for (int i = 0; i < se.length; i++) {
+				out.println(prefix + se.toString());
+			}
+		}
+	}
+}
---
+++ src/de/muenchen/allg/itd51/wollmux/VisibleTextFragmentList.java
@@ -0,0 +1,190 @@
+/*
+ * Dateiname: VisibleTextFragmentList.java
+ * Projekt  : WollMux
+ * Funktion : Repräsentiert die ausgewertete Liste aller in wollmux.conf definierten
+ *            Textfragmente.
+ * 
+ * Copyright: Landeshauptstadt München
+ *
+ * Änderungshistorie:
+ * Datum      | Wer | Änderungsgrund
+ * -------------------------------------------------------------------
+ * 13.10.2005 | LUT | Erstellung
+ * -------------------------------------------------------------------
+ *
+ * @author Christoph Lutz (D-III-ITD 5.1)
+ * @version 1.0
+ * 
+ */
+package de.muenchen.allg.itd51.wollmux;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import de.muenchen.allg.itd51.parser.ConfigThingy;
+
+/**
+ * Die VisibleTextFragmentList repräsentiert die ausgewertete Liste aller in den
+ * Konfigurationsdateien beschriebener "Textfragments" Abschnitte. Sie kümmert
+ * sich insbesondere um das Auswerten der Variablen in den URL-Attributen und um
+ * die Beachtung der Vorrangregelung: Immer das zuletzt definierte Textfragment
+ * oder die zuletzt definierte Variable gewinnt.
+ * 
+ * @author Christoph Lutz (D-III-ITD 5.1)
+ * 
+ */
+public class VisibleTextFragmentList {
+
+	/**
+	 * Speichert die Wurzel des Konfigurationsbaumes, um später Variablen
+	 * auflösen zu können.
+	 */
+	private ConfigThingy root;
+
+	/**
+	 * Die fragmentMap (of ConfigThingy) enthält alle sichtbaren Textfragmente.
+	 */
+	private Map fragmentMap;
+
+	/**
+	 * Der Konstruktor erzeugt eine neue VisibleTextFragmentList aus einer
+	 * gegebenen Konfiguration.
+	 * 
+	 * @param root
+	 *            Wurzel des Konfigurationsbaumes der Konfigurationsdatei.
+	 */
+	public VisibleTextFragmentList(ConfigThingy root) {
+		this.root = root;
+		this.fragmentMap = new HashMap();
+		ConfigThingy tfrags = root.query("Textfragments").getByChild("ID");
+		Iterator s = tfrags.iterator();
+		while (s.hasNext()) {
+			ConfigThingy frag = (ConfigThingy) s.next();
+			fragmentMap.put(frag.get("ID").toString(), frag);
+		}
+	}
+
+	private String expandVariable(ConfigThingy node, ConfigThingy root) {
+		// Map der sichtbaren Variablen erzeugen:
+		Map variables = new HashMap();
+		Iterator i = ConfigThingy.getNodesVisibleAt(node, "VAR", root)
+				.iterator();
+		while (i.hasNext()) {
+			ConfigThingy var = (ConfigThingy) i.next();
+			variables.put(var.get("NAME").toString(), var.get("VALUE")
+					.toString());
+		}
+
+		// Debug-Ausgabe:
+		Logger.debug2("Variablenset an Knoten " + node.getName() + " \""
+				+ node.toString() + "\":");
+		Iterator keys = variables.keySet().iterator();
+		while (keys.hasNext()) {
+			String key = (String) keys.next();
+			String value = (String) variables.get(key);
+			Logger.debug2("  " + key + "=\"" + value + "\"");
+		}
+
+		// Die Variable ersetzen:
+		String string = node.toString();
+		Matcher m = Pattern.compile("\\$\\{([^\\}]*)\\}").matcher(string);
+		while (m.find()) {
+			String key = m.group(1);
+			if (variables.containsKey(key)) {
+				string = m.replaceFirst((String) variables.get(key));
+				Logger.debug2("  Ersetzen der Variable " + m.group(0) + " --> "
+						+ string);
+			} else {
+				// Die Variable kann nicht ersetzt werden und wird auch nicht
+				// ersetzt.
+				// Eine Exception muss deswegen nicht geworfen werden, es ist
+				// aber
+				// sinnvoll, die Fehlermeldung in einem Logger rauszuschreiben.
+				Logger.log("Warnung: Die Variable \"" + key
+						+ "\" in der URL \"" + string
+						+ "\" ist nicht definiert.");
+			}
+		}
+		return string;
+	}
+
+	/**
+	 * Gibt die URL des mit id definierten Textfragments zurück. Falls das
+	 * Textfragment nicht existiert, wird null zurückgeliefert.
+	 * 
+	 * @param id
+	 *            Die ID des gesuchten Textfragments.
+	 * @return die URL des mit id definierten Textfragments. Falls das
+	 *         Textfragment nicht existiert oder die URL ungültig ist, wird null
+	 *         zurückgeliefert.
+	 * @throws MalformedURLException
+	 *             Falls die URL fehlerhaft ist.
+	 */
+	public URL getURLByID(String id) {
+		if (id != null && fragmentMap.containsKey(id)) {
+			ConfigThingy frag = (ConfigThingy) fragmentMap.get(id);
+			try {
+				// TODO: Die URL kann noch keine relativen Pfade ohne
+				// Protokoll...
+				return new URL(expandVariable(frag.get("URL"), root));
+			} catch (MalformedURLException e) {
+				Logger.log(e);
+				return null;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Diese Methode erzeugt ein String-Array der IDs aller erkannten
+	 * Textfragmente.
+	 * 
+	 * @return Ein String-Array der IDs aller erkannten Textfragmente.
+	 */
+	public String[] getIDs() {
+		Set keys = fragmentMap.keySet();
+		return (String[]) keys.toArray(new String[keys.size()]);
+	}
+
+	/**
+	 * Testet die Funktionsweise der VisibleTextFragmentList. Eine in url
+	 * angegebene Konfigdatei wird eingelesen und die dazugehörige
+	 * VisibleTextFragmentList erstellt. Anschliessend wird die ausgegeben.
+	 * 
+	 * @param args
+	 *            url, dabei ist url die URL einer zu lesenden Config-Datei. Das
+	 *            Programm gibt die Liste der Textfragmente aus.
+	 * @author Christoph Lutz (D-III-ITD 5.1)
+	 */
+	public static void main(String[] args) throws IOException {
+		if (args.length < 1) {
+			System.out.println("USAGE: <url>");
+			System.exit(0);
+		}
+		Logger.init(System.out, Logger.DEBUG);
+
+		File cwd = new File(".");
+
+		args[0] = args[0].replaceAll("\\\\", "/");
+		ConfigThingy conf = new ConfigThingy(args[0], new URL(cwd.toURL(),
+				args[0]));
+
+		VisibleTextFragmentList tfrags = new VisibleTextFragmentList(conf);
+
+		String[] ids = tfrags.getIDs();
+		for (int i = 0; i < ids.length; i++) {
+			Logger.debug("Textfragment: " + ids[i] + " --> "
+					+ tfrags.getURLByID(ids[i]));
+		}
+
+		System.exit(0);
+	}
+}
