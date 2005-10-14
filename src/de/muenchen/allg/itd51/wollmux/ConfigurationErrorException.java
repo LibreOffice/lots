c5fd30ab7a68a43e2d8cf9bbc6f62e1af1dda9b0
bnk <devnull@localhost>
2005-10-14 08:07:41 +0200

    [svn r188] (BNK)keine RuntimeException mehr

--- src/de/muenchen/allg/itd51/wollmux/ConfigurationErrorException.java
+++ src/de/muenchen/allg/itd51/wollmux/ConfigurationErrorException.java
@@ -10,6 +10,7 @@
 * -------------------------------------------------------------------
 * 11.10.2005 | BNK | Erstellung
 * 13.10.2005 | BNK | +serialVersionUID
+* 14.10.2005 | BNK | keine RuntimeException mehr
 * -------------------------------------------------------------------
 *
 * @author Matthias Benkmann (D-III-ITD 5.1)
@@ -22,7 +23,7 @@ package de.muenchen.allg.itd51.wollmux;
  * wird geworfen, wenn eine Fehlkonfiguration festgestellt wird (d.h. Benutzer hat Config verbockt)
  * @author Matthias Benkmann (D-III-ITD 5.1)
  */
-public class ConfigurationErrorException extends RuntimeException
+public class ConfigurationErrorException extends Exception
 {
   /**
    * keine Ahnung, was das soll, aber es macht Eclipse glücklich.
