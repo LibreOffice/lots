a46273d3610e8bdb0412eca2814f175703be5662
bnk <devnull@localhost>
2005-10-14 09:14:04 +0200

    [svn r194] (BNK) ->Interface

--- src/de/muenchen/allg/itd51/wollmux/db/Dataset.java
+++ src/de/muenchen/allg/itd51/wollmux/db/Dataset.java
@@ -9,6 +9,7 @@
 * Datum      | Wer | Änderungsgrund
 * -------------------------------------------------------------------
 * 06.10.2005 | BNK | Erstellung
+* 14.10.2005 | BNK | ->Interface
 * -------------------------------------------------------------------
 *
 * @author Matthias Benkmann (D-III-ITD 5.1)
@@ -21,10 +22,7 @@ package de.muenchen.allg.itd51.wollmux.db;
  * TODO Doku
  * @author Matthias Benkmann (D-III-ITD 5.1)
  */
-public class Dataset
+public interface Dataset
 {
-  public String get(String spaltenName)
-  {
-    return null;
-  }
+  public String get(String spaltenName);
 }
