696cd175196f03ad027962e447c6c897bb69bdbd
bnk <devnull@localhost>
2005-10-28 14:17:51 +0200

    [svn r273] (BNK) TODO weg

--- src/de/muenchen/allg/itd51/wollmux/db/Dataset.java
+++ src/de/muenchen/allg/itd51/wollmux/db/Dataset.java
@@ -20,10 +20,28 @@
 package de.muenchen.allg.itd51.wollmux.db;
 
 /**
- * TODO Doku
+ * Interface für Datensätze einer Tabelle.
  * @author Matthias Benkmann (D-III-ITD 5.1)
  */
 public interface Dataset
 {
+  /**
+   * Liefert den Wert des Datensatzes aus der Spalte  columnName (null
+   * falls nicht belegt).
+   * @throws ColumnNotFoundException, falls die Spalte nicht existiert. 
+   * Man beachte, dass dies eine Eigenschaft des Datenbankschemas ist und
+   * nichts damit zu tun hat, ob der Wert des Datensatzes in der 
+   * entsprechenden Spalte gesetzt ist.
+   * @author Matthias Benkmann (D-III-ITD 5.1)
+   */
   public String get(String columnName) throws ColumnNotFoundException;
+  
+  /**
+   * Liefert den Schlüsselwert dieses Datensatzes. Dieser sollte den
+   * Datensatz in seiner Datenbank eindeutig identifizieren muss es aber
+   * nicht.
+   * 
+   * @author Matthias Benkmann (D-III-ITD 5.1)
+   */
+  public String getKey();
 }
