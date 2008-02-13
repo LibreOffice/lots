e29b3492bb62f3ba59ce24b6495924419a8866f2
bnk <devnull@localhost>
2008-02-13 10:05:48 +0100

    [svn r2393] (BNK)JPotentiallyOverlongPopupMenuButton fertiggestellt (minus seiner Hauptfunktionalität ;-)

--- src/de/muenchen/allg/itd51/wollmux/dialog/JPotentiallyOverlongPopupMenuButton.java
+++ src/de/muenchen/allg/itd51/wollmux/dialog/JPotentiallyOverlongPopupMenuButton.java
@@ -17,31 +17,60 @@
 */
 package de.muenchen.allg.itd51.wollmux.dialog;
 
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Iterator;
 import java.util.List;
 
+import javax.swing.Action;
 import javax.swing.JButton;
+import javax.swing.JPopupMenu;
 
 /**
  * Stellt einen Button dar mit einem Popup-Menü, das darauf vorbereitet ist,
  * sehr viele Elemente anzubieten.
+ * TODO: Das Behandeln von überlangen Menüs muss noch implementiert werden.
  * 
  * @author Matthias Benkmann (D-III-ITD D.10)
  */
 public class JPotentiallyOverlongPopupMenuButton extends JButton
 {
   /**
+   * Macht Eclipse glücklich.
+   */
+  private static final long serialVersionUID = 3206786778925266706L;
+
+  /**
    * Erzeugt einen Button mit Beschriftung label, bei dessen Betätigung
    * eine Popup-Menü erscheint, in dem alle Elemente aus actions enthalten sind.
-   * Die Elemente von actions sollten {@link javax.swing.Action} Objekte sein,
-   * es sind jedoch alle Elemente erlaubt, die man in ein Menü stecken kann,
-   * z.B. Separatoren.
-   * @param label
-   * @param actions
+   * Wenn das Popup-Menü zu lang wäre, um auf den Bildschirm zu passen, passiert
+   * etwas intelligentes.
+   * Die Elemente von actions können {@link javax.swing.Action} oder
+   * {@link java.awt.Component} Objekte sein.
    * @author Matthias Benkmann (D-III-ITD D.10)
-   * TODO Testen
    */
   public JPotentiallyOverlongPopupMenuButton(String label, List actions)
   {
+    super(label);
+    final JPopupMenu menu = new JPopupMenu();
     
+    Iterator iter = actions.iterator();
+    while (iter.hasNext())
+    {
+      Object action = iter.next();
+      if (action instanceof Action)
+        menu.add((Action)action);
+      else
+        menu.add((Component)action);
+    }
+    
+    this.addActionListener(new ActionListener()
+    {
+      public void actionPerformed(ActionEvent e)
+      {
+        menu.show(JPotentiallyOverlongPopupMenuButton.this, 0, JPotentiallyOverlongPopupMenuButton.this.getSize().height);
+      }
+    });
   }
 }
