344e90d0bbe34cadb615b8f38151a4eadb45b3ec
bnk <devnull@localhost>
2008-02-13 11:03:49 +0100

    [svn r2395] (BNK)organize imports

--- src/de/muenchen/allg/itd51/wollmux/dialog/JPotentiallyOverlongPopupMenuButton.java
+++ src/de/muenchen/allg/itd51/wollmux/dialog/JPotentiallyOverlongPopupMenuButton.java
@@ -21,7 +21,6 @@ import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Iterator;
-import java.util.List;
 
 import javax.swing.Action;
 import javax.swing.JButton;
