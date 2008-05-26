5ad2ccb9a065027fbd3de64d28286744f5aeda3d
bnk <devnull@localhost>
2008-05-26 08:44:06 +0200

    [svn r2630] (BNK)L.m() und generifiziert

--- src/de/muenchen/allg/itd51/wollmux/former/BroadcastObjectSelectionByBookmarks.java
+++ src/de/muenchen/allg/itd51/wollmux/former/BroadcastObjectSelectionByBookmarks.java
@@ -1,9 +1,8 @@
-//TODO L.m()
 /*
-* Dateiname: BroadcastObjectSelectionByBookmarks.java
-* Projekt  : WollMux
-* Funktion : Nachricht, dass eine Menge von Bookmarks selektiert wurde.
-* 
+ * Dateiname: BroadcastObjectSelectionByBookmarks.java
+ * Projekt  : WollMux
+ * Funktion : Nachricht, dass eine Menge von Bookmarks selektiert wurde.
+ * 
  * Copyright (c) 2008 Landeshauptstadt München
  *
  * This program is free software: you can redistribute it and/or modify
@@ -18,26 +17,26 @@
  * You should have received a copy of the European Union Public Licence
  * along with this program. If not, see
  * http://ec.europa.eu/idabc/en/document/7330
-*
-* Änderungshistorie:
-* Datum      | Wer | Änderungsgrund
-* -------------------------------------------------------------------
-* 31.10.2006 | BNK | Erstellung
-* -------------------------------------------------------------------
-*
-* @author Matthias Benkmann (D-III-ITD 5.1)
-* @version 1.0
-* 
-*/
+ *
+ * Änderungshistorie:
+ * Datum      | Wer | Änderungsgrund
+ * -------------------------------------------------------------------
+ * 31.10.2006 | BNK | Erstellung
+ * -------------------------------------------------------------------
+ *
+ * @author Matthias Benkmann (D-III-ITD 5.1)
+ * @version 1.0
+ * 
+ */
 package de.muenchen.allg.itd51.wollmux.former;
 
 import java.util.Set;
 
 public class BroadcastObjectSelectionByBookmarks implements Broadcast
 {
-  private Set bookmarkNames;
-  
-  public BroadcastObjectSelectionByBookmarks(Set bookmarkNames)
+  private Set<String> bookmarkNames;
+
+  public BroadcastObjectSelectionByBookmarks(Set<String> bookmarkNames)
   {
     this.bookmarkNames = bookmarkNames;
   }
