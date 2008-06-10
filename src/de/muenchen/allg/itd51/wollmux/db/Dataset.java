4611609ac5dfb2a242ddc67f054ddebafc21ea89
bnk <devnull@localhost>
2008-06-10 09:24:36 +0200

    [svn r2665] (BNK)L.m()

--- src/de/muenchen/allg/itd51/wollmux/db/Dataset.java
+++ src/de/muenchen/allg/itd51/wollmux/db/Dataset.java
@@ -1,9 +1,8 @@
-//TODO L.m()
 /*
-* Dateiname: Dataset.java
-* Projekt  : WollMux
-* Funktion : Interface für Datensätze einer Tabelle.
-* 
+ * Dateiname: Dataset.java
+ * Projekt  : WollMux
+ * Funktion : Interface für Datensätze einer Tabelle.
+ * 
  * Copyright (c) 2008 Landeshauptstadt München
  *
  * This program is free software: you can redistribute it and/or modify
@@ -18,42 +17,43 @@
  * You should have received a copy of the European Union Public Licence
  * along with this program. If not, see
  * http://ec.europa.eu/idabc/en/document/7330
-*
-* Änderungshistorie:
-* Datum      | Wer | Änderungsgrund
-* -------------------------------------------------------------------
-* 06.10.2005 | BNK | Erstellung
-* 14.10.2005 | BNK | ->Interface
-* 14.10.2005 | BNK | get() throws ColumnNotFoundException
-* -------------------------------------------------------------------
-*
-* @author Matthias Benkmann (D-III-ITD 5.1)
-* @version 1.0
-* 
-*/
+ *
+ * Änderungshistorie:
+ * Datum      | Wer | Änderungsgrund
+ * -------------------------------------------------------------------
+ * 06.10.2005 | BNK | Erstellung
+ * 14.10.2005 | BNK | ->Interface
+ * 14.10.2005 | BNK | get() throws ColumnNotFoundException
+ * -------------------------------------------------------------------
+ *
+ * @author Matthias Benkmann (D-III-ITD 5.1)
+ * @version 1.0
+ * 
+ */
 package de.muenchen.allg.itd51.wollmux.db;
 
 /**
  * Interface für Datensätze einer Tabelle.
+ * 
  * @author Matthias Benkmann (D-III-ITD 5.1)
  */
 public interface Dataset
 {
   /**
-   * Liefert den Wert des Datensatzes aus der Spalte  columnName (null
-   * falls nicht belegt).
-   * @throws ColumnNotFoundException, falls die Spalte nicht existiert. 
-   * Man beachte, dass dies eine Eigenschaft des Datenbankschemas ist und
-   * nichts damit zu tun hat, ob der Wert des Datensatzes in der 
-   * entsprechenden Spalte gesetzt ist.
+   * Liefert den Wert des Datensatzes aus der Spalte columnName (null falls nicht
+   * belegt).
+   * 
+   * @throws ColumnNotFoundException,
+   *           falls die Spalte nicht existiert. Man beachte, dass dies eine
+   *           Eigenschaft des Datenbankschemas ist und nichts damit zu tun hat, ob
+   *           der Wert des Datensatzes in der entsprechenden Spalte gesetzt ist.
    * @author Matthias Benkmann (D-III-ITD 5.1)
    */
   public String get(String columnName) throws ColumnNotFoundException;
-  
+
   /**
-   * Liefert den Schlüsselwert dieses Datensatzes. Dieser sollte den
-   * Datensatz in seiner Datenbank eindeutig identifizieren muss es aber
-   * nicht.
+   * Liefert den Schlüsselwert dieses Datensatzes. Dieser sollte den Datensatz in
+   * seiner Datenbank eindeutig identifizieren muss es aber nicht.
    * 
    * @author Matthias Benkmann (D-III-ITD 5.1)
    */
