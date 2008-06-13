6d45a73583b6cb38e347c98c53363076deb2a1d6
bnk <devnull@localhost>
2008-06-13 09:01:41 +0200

    [svn r2671] (BNK)L.m()

--- src/de/muenchen/allg/itd51/wollmux/former/IndexList.java
+++ src/de/muenchen/allg/itd51/wollmux/former/IndexList.java
@@ -1,9 +1,8 @@
-//TODO L.m()
 /*
-* Dateiname: IndexList.java
-* Projekt  : WollMux
-* Funktion : Verwaltet eine Liste von Indices.
-* 
+ * Dateiname: IndexList.java
+ * Projekt  : WollMux
+ * Funktion : Verwaltet eine Liste von Indices.
+ * 
  * Copyright (c) 2008 Landeshauptstadt München
  *
  * This program is free software: you can redistribute it and/or modify
@@ -18,17 +17,17 @@
  * You should have received a copy of the European Union Public Licence
  * along with this program. If not, see
  * http://ec.europa.eu/idabc/en/document/7330
-*
-* Änderungshistorie:
-* Datum      | Wer | Änderungsgrund
-* -------------------------------------------------------------------
-* 29.09.2006 | BNK | Erstellung
-* -------------------------------------------------------------------
-*
-* @author Matthias Benkmann (D-III-ITD 5.1)
-* @version 1.0
-* 
-*/
+ *
+ * Änderungshistorie:
+ * Datum      | Wer | Änderungsgrund
+ * -------------------------------------------------------------------
+ * 29.09.2006 | BNK | Erstellung
+ * -------------------------------------------------------------------
+ *
+ * @author Matthias Benkmann (D-III-ITD 5.1)
+ * @version 1.0
+ * 
+ */
 package de.muenchen.allg.itd51.wollmux.former;
 
 import java.util.Collections;
@@ -41,13 +40,13 @@ public class IndexList
    * Die (immer aufsteigend sortierte) Liste der Indizes.
    */
   private Vector<Integer> indices = new Vector<Integer>();
-  
-  
+
   /**
-   * Addiert auf alle Indizes in der {@link #indices} Liste größer gleich start den offset.
-   * Indizes die dabei < 0 oder > maxindex werden werden aus der Liste gelöscht.
-   * @author Matthias Benkmann (D-III-ITD 5.1)
-   * TESTED
+   * Addiert auf alle Indizes in der {@link #indices} Liste größer gleich start den
+   * offset. Indizes die dabei < 0 oder > maxindex werden werden aus der Liste
+   * gelöscht.
+   * 
+   * @author Matthias Benkmann (D-III-ITD 5.1) TESTED
    */
   public void fixup(int start, int offset, int maxindex)
   {
@@ -61,16 +60,16 @@ public class IndexList
         i = i + offset;
         if (i < 0 || i > maxindex)
           iter.remove();
-        else 
+        else
           iter.set(new Integer(i));
       }
     }
   }
-  
+
   /**
    * Ersetzt in {@link #indices} index1 durch index2 und index2 durch index1.
-   * @author Matthias Benkmann (D-III-ITD 5.1)
-   * TESTED
+   * 
+   * @author Matthias Benkmann (D-III-ITD 5.1) TESTED
    */
   public void swap(int index1, int index2)
   {
@@ -81,75 +80,74 @@ public class IndexList
       int i = I.intValue();
       if (i == index1)
         iter.set(new Integer(index2));
-      else if (i == index2)
-        iter.set(new Integer(index1));
+      else if (i == index2) iter.set(new Integer(index1));
     }
-    
+
     Collections.sort(indices);
   }
 
   /**
    * Liefert einen Iterator über die Integers in dieser Liste.
+   * 
    * @author Matthias Benkmann (D-III-ITD 5.1)
    */
   public ListIterator<Integer> iterator()
   {
     return indices.listIterator();
   }
-  
+
   /**
-   * Liefert einen Listiterator, der hinter dem letzten Element der Liste (von Integers) startet.
+   * Liefert einen Listiterator, der hinter dem letzten Element der Liste (von
+   * Integers) startet.
+   * 
    * @author Matthias Benkmann (D-III-ITD 5.1)
    */
   public ListIterator<Integer> reverseIterator()
   {
     return indices.listIterator(indices.size());
   }
-  
+
   /**
    * Entfernt den Index i aus der {@link #indices} Liste falls er dort enthalten ist.
-   * @author Matthias Benkmann (D-III-ITD 5.1)
-   * TESTED*/
+   * 
+   * @author Matthias Benkmann (D-III-ITD 5.1) TESTED
+   */
   public void remove(int i)
   {
-    if (indices.remove(new Integer(i)))
-      Collections.sort(indices);
+    if (indices.remove(new Integer(i))) Collections.sort(indices);
   }
-  
+
   public void add(int i)
   {
     indices.add(new Integer(i));
     Collections.sort(indices);
   }
-  
+
   public boolean contains(int i)
   {
     return indices.contains(new Integer(i));
   }
-  
+
   public boolean isEmpty()
   {
     return indices.isEmpty();
   }
-  
-  
-  
+
   public int firstElement()
   {
     if (indices.size() == 0) return -1;
-    return indices.firstElement().intValue(); 
+    return indices.firstElement().intValue();
   }
-  
+
   public int lastElement()
   {
     if (indices.size() == 0) return -1;
-    return indices.lastElement().intValue(); 
+    return indices.lastElement().intValue();
   }
-  
+
   public void clear()
   {
     indices.clear();
   }
-  
 
 }
