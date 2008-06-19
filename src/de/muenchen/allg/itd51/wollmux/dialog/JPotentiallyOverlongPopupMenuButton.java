92ae07820546c41ffd5b1eba64faef21e4510ee1
bnk <devnull@localhost>
2008-06-19 16:11:01 +0200

    [svn r2683] (BNK)implemented handling of overlong menus in JPotentiallyOverlongPopupMenuButton

--- src/de/muenchen/allg/itd51/wollmux/dialog/JPotentiallyOverlongPopupMenuButton.java
+++ src/de/muenchen/allg/itd51/wollmux/dialog/JPotentiallyOverlongPopupMenuButton.java
@@ -22,6 +22,7 @@
  * Datum      | Wer | Änderungsgrund
  * -------------------------------------------------------------------
  * 13.02.2008 | BNK | Erstellung
+ * 19.06.2008 | BNK | Implemented handling of overlong menus.
  * -------------------------------------------------------------------
  *
  * @author Matthias Benkmann (D-III-ITD D.10)
@@ -31,18 +32,28 @@
 package de.muenchen.allg.itd51.wollmux.dialog;
 
 import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Iterator;
+import java.util.Vector;
 
+import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPopupMenu;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
 
 /**
  * Stellt einen Button dar mit einem Popup-Menü, das darauf vorbereitet ist, sehr
- * viele Elemente anzubieten. TODO: Das Behandeln von überlangen Menüs muss noch
- * implementiert werden.
+ * viele Elemente anzubieten.
  * 
  * @author Matthias Benkmann (D-III-ITD D.10)
  */
@@ -73,6 +84,7 @@ public class JPotentiallyOverlongPopupMenuButton extends JButton
       public void actionPerformed(ActionEvent e)
       {
         JPopupMenu menu = new JPopupMenu();
+        menu.setLayout(new MyVerticalFlowLayout());
         Iterator<?> iter = actions.iterator();
         while (iter.hasNext())
         {
@@ -81,11 +93,62 @@ public class JPotentiallyOverlongPopupMenuButton extends JButton
             menu.add((Action) action);
           else
             menu.add((Component) action);
+
+          // menu.invalidate();
+          // System.out.println(menu.getPreferredSize());
+        }
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Component compo = JPotentiallyOverlongPopupMenuButton.this;
+        Point p =
+          new Point(0, JPotentiallyOverlongPopupMenuButton.this.getSize().height);
+        SwingUtilities.convertPointToScreen(p, compo);
+        int maxheight = screenSize.height - p.y;
+        if (maxheight < 1) maxheight = 1;
+        menu.setMaximumSize(new Dimension(screenSize.width, maxheight));
+        menu.invalidate();
+        Dimension d = menu.getMinimumSize();
+        if (d.width > screenSize.width)
+        {
+          menu.setMaximumSize(new Dimension(screenSize.width, screenSize.height));
+          menu.invalidate();
         }
 
         menu.show(JPotentiallyOverlongPopupMenuButton.this, 0,
           JPotentiallyOverlongPopupMenuButton.this.getSize().height);
       }
     });
+  }
+
+  private static class MyVerticalFlowLayout extends VerticalFlowLayout
+  {}
+
+  public static void main(String[] args)
+  {
+    JFrame myFrame = new JFrame("JPotentiallyOverlongPopupMenuButtonTest");
+    myFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    final Box myPanel = new Box(BoxLayout.X_AXIS);
+    myFrame.setContentPane(myPanel);
+
+    Vector<Action> actions = new Vector<Action>();
+    for (int i = 1; i <= 100; ++i)
+    {
+      final Integer I = new Integer(i);
+      actions.add(new AbstractAction("" + i)
+      {
+
+        public void actionPerformed(ActionEvent e)
+        {
+          JOptionPane.showMessageDialog(myPanel, "" + I);
+        }
+      });
+    }
+    JPotentiallyOverlongPopupMenuButton butt =
+      new JPotentiallyOverlongPopupMenuButton(
+        "JPotentiallyOverlongPopupMenuButtonTest", actions);
+
+    myPanel.add(butt);
+    myFrame.pack();
+    myFrame.setVisible(true);
   }
 }
---
+++ src/de/muenchen/allg/itd51/wollmux/dialog/VerticalFlowLayout.java
@@ -0,0 +1,171 @@
+/*
+ * Dateiname: VerticalFlowLayout.java
+ * Projekt  : WollMux
+ * Funktion : Similar to FlowLayout but using vertical arrangement.
+ * 
+ * Copyright (c) 2008 Landeshauptstadt München
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the European Union Public Licence (EUPL), 
+ * version 1.0.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * European Union Public Licence for more details.
+ *
+ * You should have received a copy of the European Union Public Licence
+ * along with this program. If not, see 
+ * http://ec.europa.eu/idabc/en/document/7330
+ *
+ * Änderungshistorie:
+ * Datum      | Wer | Änderungsgrund
+ * -------------------------------------------------------------------
+ * 19.06.2008 | BNK | Erstellung
+ * -------------------------------------------------------------------
+ *
+ * @author Matthias Benkmann (D-III-ITD D.10)
+ * @version 1.0
+ * 
+ */
+package de.muenchen.allg.itd51.wollmux.dialog;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Insets;
+import java.awt.LayoutManager;
+
+/**
+ * Similar to {@link FlowLayout} but using vertical arrangement. The height of the
+ * layout will be the maximum height of the container.
+ * 
+ * @author Matthias Benkmann (D-III-ITD D.10)
+ */
+public class VerticalFlowLayout implements LayoutManager
+{
+
+  public void addLayoutComponent(String name, Component comp)
+  {}
+
+  public void removeLayoutComponent(Component comp)
+  {}
+
+  public void layoutContainer(Container parent)
+  {
+    synchronized (parent.getTreeLock())
+    {
+      Insets insets = parent.getInsets();
+      int maxheight = parent.getHeight() - insets.bottom - insets.top;
+
+      int count = parent.getComponentCount();
+
+      int x = insets.left;
+      int y = insets.top;
+      int height = 0;
+      int columnWidth = 0;
+      boolean firstInColumn = true;
+
+      for (int i = 0; i < count; ++i)
+      {
+        Component compo = parent.getComponent(i);
+        Dimension pref = compo.getPreferredSize();
+
+        // Do not use setSize(Dimension), because it might store the object reference
+        // directly
+        compo.setSize(pref.width, pref.height);
+
+        /*
+         * Start new column if the component doesn't fit into the current column
+         * anymore, but don't do this if it's the 1st component in the column,
+         * because in that case starting a new column won't help either.
+         */
+        if (!firstInColumn && (height + pref.height > maxheight))
+        {
+          x += columnWidth;
+          y = insets.top;
+          columnWidth = 0;
+          height = 0;
+          firstInColumn = true;
+        }
+
+        firstInColumn = false;
+
+        compo.setLocation(x, y);
+        if (pref.width > columnWidth) columnWidth = pref.width;
+        height += pref.height;
+        y += pref.height;
+      }
+
+    }
+  }
+
+  public Dimension minimumLayoutSize(Container parent)
+  {
+    synchronized (parent.getTreeLock())
+    {
+      /*
+       * Don't ask me why, but in the case of a JPopupMenu (see
+       * JPotentiallyOverlongPopupMenuButton) the individual components don't always
+       * start reporting their correct preferred sizes until after the last element's
+       * preferred size has been accessed.
+       * 
+       * With JPotentially... this can be observed when the button is moved to the
+       * bottom of the screen so that the menu will no longer fit under the button
+       * because it's just a little too wide. In that case, rather than switch to
+       * making the menu vertically large, the menu extends beyond the screen border
+       * to the right. The following statement fixes this.
+       * 
+       * So just leave it in. It doesn't hurt.
+       */
+      parent.getComponent(parent.getComponentCount() - 1).getPreferredSize();
+
+      Insets insets = parent.getInsets();
+      int maxheight = parent.getMaximumSize().height - insets.bottom - insets.top;
+      int containerHeight = 0;
+      int containerWidth = 0;
+
+      int count = parent.getComponentCount();
+
+      int columnHeight = 0;
+      int columnWidth = 0;
+      boolean firstInColumn = true;
+
+      for (int i = 0; i < count; ++i)
+      {
+        Component compo = parent.getComponent(i);
+        Dimension pref = compo.getPreferredSize();
+        /*
+         * Start new column if the component doesn't fit into the current column
+         * anymore, but don't do this if it's the 1st component in the column,
+         * because in that case starting a new column won't help either.
+         */
+        if (!firstInColumn && (columnHeight + pref.height > maxheight))
+        {
+          containerWidth += columnWidth;
+          if (containerHeight < columnHeight) containerHeight = columnHeight;
+          columnWidth = 0;
+          columnHeight = 0;
+          firstInColumn = true;
+        }
+
+        firstInColumn = false;
+
+        if (pref.width > columnWidth) columnWidth = pref.width;
+        columnHeight += pref.height;
+      }
+
+      containerWidth += columnWidth;
+      if (containerHeight < columnHeight) containerHeight = columnHeight;
+      return new Dimension(containerWidth + insets.left + insets.right,
+        containerHeight + insets.top + insets.bottom);
+    }
+  }
+
+  public Dimension preferredLayoutSize(Container parent)
+  {
+    return minimumLayoutSize(parent);
+  }
+
+}
