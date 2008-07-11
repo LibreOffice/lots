271c119040e689e2a08817c22a26cec6a3d8003e
bnk <devnull@localhost>
2008-07-11 11:48:08 +0200

    [svn r2733] (BNK)"Felder anpassen" und Verwandte nach AdjustFields ausgelagert

--- src/de/muenchen/allg/itd51/wollmux/dialog/mailmerge/MailMergeNew.java
+++ src/de/muenchen/allg/itd51/wollmux/dialog/mailmerge/MailMergeNew.java
@@ -32,22 +32,17 @@ package de.muenchen.allg.itd51.wollmux.dialog.mailmerge;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -67,9 +62,7 @@ import javax.swing.JLabel;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.JRadioButton;
-import javax.swing.JScrollPane;
 import javax.swing.JTextField;
-import javax.swing.ScrollPaneConstants;
 import javax.swing.WindowConstants;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
@@ -88,16 +81,12 @@ import de.muenchen.allg.itd51.wollmux.TextDocumentModel;
 import de.muenchen.allg.itd51.wollmux.UnavailableException;
 import de.muenchen.allg.itd51.wollmux.WollMuxSingleton;
 import de.muenchen.allg.itd51.wollmux.XPrintModel;
-import de.muenchen.allg.itd51.wollmux.TextDocumentModel.FieldSubstitution;
-import de.muenchen.allg.itd51.wollmux.TextDocumentModel.ReferencedFieldID;
 import de.muenchen.allg.itd51.wollmux.db.Dataset;
 import de.muenchen.allg.itd51.wollmux.db.MailMergeDatasource;
 import de.muenchen.allg.itd51.wollmux.db.QueryResults;
 import de.muenchen.allg.itd51.wollmux.db.QueryResultsWithSchema;
 import de.muenchen.allg.itd51.wollmux.dialog.DimAdjust;
 import de.muenchen.allg.itd51.wollmux.dialog.JPotentiallyOverlongPopupMenuButton;
-import de.muenchen.allg.itd51.wollmux.dialog.TextComponentTags;
-import de.muenchen.allg.itd51.wollmux.dialog.TextComponentTags.ContentElement;
 import de.muenchen.allg.itd51.wollmux.dialog.trafo.GenderDialog;
 import de.muenchen.allg.itd51.wollmux.dialog.trafo.TrafoDialog;
 import de.muenchen.allg.itd51.wollmux.dialog.trafo.TrafoDialogFactory;
@@ -479,7 +468,7 @@ public class MailMergeNew
     {
       public void actionPerformed(ActionEvent e)
       {
-        showAddMissingColumnsDialog();
+        AdjustFields.showAddMissingColumnsDialog(myFrame, mod, ds);
       }
     });
     tabelleMenu.add(addColumnsMenuItem);
@@ -490,7 +479,7 @@ public class MailMergeNew
     {
       public void actionPerformed(ActionEvent e)
       {
-        showAdjustFieldsDialog();
+        AdjustFields.showAdjustFieldsDialog(myFrame, mod, ds);
       }
     });
     tabelleMenu.add(adjustFieldsMenuItem);
@@ -582,297 +571,6 @@ public class MailMergeNew
       mod.updateFormFields(column);
     }
 
-  }
-
-  /**
-   * Diese Methode zeigt den Dialog an, mit dem die Felder im Dokument an eine
-   * Datenquelle angepasst werden können, die nicht die selben Spalten enthält wie
-   * die Datenquelle, für die das Dokument gemacht wurde.
-   * 
-   * @author Christoph Lutz (D-III-ITD-5.1)
-   */
-  protected void showAdjustFieldsDialog()
-  {
-    ReferencedFieldID[] fieldIDs =
-      mod.getReferencedFieldIDsThatAreNotInSchema(new HashSet<String>(
-        ds.getColumnNames()));
-    ActionListener submitActionListener = new ActionListener()
-    {
-      @SuppressWarnings("unchecked")
-      public void actionPerformed(ActionEvent e)
-      {
-        HashMap<String, FieldSubstitution> mapIdToSubstitution =
-          (HashMap<String, FieldSubstitution>) e.getSource();
-        for (Iterator<String> iter = mapIdToSubstitution.keySet().iterator(); iter.hasNext();)
-        {
-          String fieldId = iter.next();
-          FieldSubstitution subst = mapIdToSubstitution.get(fieldId);
-          mod.applyFieldSubstitution(fieldId, subst);
-        }
-      }
-    };
-    showFieldMappingDialog(fieldIDs, L.m("Felder anpassen"), L.m("Altes Feld"),
-      L.m("Neue Belegung"), L.m("Felder anpassen"), submitActionListener);
-  }
-
-  /**
-   * Diese Methode zeigt den Dialog an, mit dem die Spalten der Tabelle ergänzt
-   * werden können, wenn es zu Feldern im Dokument keine passenden Spalten in der
-   * Tabelle gibt.
-   * 
-   * @author Christoph Lutz (D-III-ITD-5.1)
-   */
-  protected void showAddMissingColumnsDialog()
-  {
-    ReferencedFieldID[] fieldIDs =
-      mod.getReferencedFieldIDsThatAreNotInSchema(new HashSet<String>(
-        ds.getColumnNames()));
-    ActionListener submitActionListener = new ActionListener()
-    {
-      public void actionPerformed(ActionEvent e)
-      {
-      // Enthält die Zuordnung ID ->
-      // de.muenchen.allg.itd51.wollmux.TextDocumentModel.FieldSubstitution,
-      // in der die anzuwendende Ersetzungsregel beschrieben ist.
-
-      // HashMap mapIdToSubstitution = (HashMap) e.getSource();
-      // TODO: tabellenspalten wie in mapIdToSubstitution beschrieben ergänzen
-      }
-    };
-    showFieldMappingDialog(fieldIDs, L.m("Tabellenspalten ergänzen"), L.m("Spalte"),
-      L.m("Vorbelegung"), L.m("Spalten ergänzen"), submitActionListener);
-  }
-
-  /**
-   * Zeigt einen Dialog mit dem bestehende Felder fieldIDs über ein Textfeld neu
-   * belegt werden können; für die neue Belegung stehen die neuen Felder der
-   * aktuellen Datasource und Freitext zur Verfügung. Die Felder fieldIDs werden
-   * dabei in der Reihenfolge angezeigt, in der sie in der Liste aufgeführt sind, ein
-   * bereits aufgeführtes Feld wird aber nicht zweimal angezeigt. Ist bei einem Feld
-   * die Eigenschaft isTransformed()==true, dann wird für dieses Feld nur die Eingabe
-   * einer 1-zu-1 Zuordnung von Feldern akzeptiert, das andere Zuordnungen für
-   * transformierte Felder derzeit nicht unterstützt werden.
-   * 
-   * @param fieldIDs
-   *          Die field-IDs der alten, bereits im Dokument enthaltenen Felder, die in
-   *          der gegebenen Reihenfolge angezeigt werden, Dupletten werden aber
-   *          entfernt.
-   * @param title
-   *          Die Titelzeile des Dialogs
-   * @param labelOldFields
-   *          Die Spaltenüberschrift für die linke Spalte, in der die alten Felder
-   *          angezeigt werden.
-   * @param labelNewFields
-   *          Die Spaltenüberschrift für die rechte Spalte, in dem die neue Zuordnung
-   *          getroffen wird.
-   * @param labelSubmitButton
-   *          Die Beschriftung des Submit-Knopfes unten rechts, der die entsprechende
-   *          Aktion auslöst.
-   * @param submitActionListener
-   *          Nach Beendigung des Dialogs über den Submit-Knopf (unten rechts) wird
-   *          die Methode submitActionListener.actionPerformed(actionEvent) in einem
-   *          separaten Thread aufgerufen. Dort kann der Code stehen, der gewünschten
-   *          Aktionen durchführt. Der ActionListener bekommt dabei in actionEvent
-   *          eine HashMap übergeben, die eine Zuordnung von den alten fieldIDs auf
-   *          den jeweiligen im Dialog gewählten Ersatzstring enthält.
-   * 
-   * @author Christoph Lutz (D-III-ITD-5.1)
-   */
-  private void showFieldMappingDialog(ReferencedFieldID[] fieldIDs, String title,
-      String labelOldFields, String labelNewFields, String labelSubmitButton,
-      final ActionListener submitActionListener)
-  {
-    final JDialog dialog = new JDialog(myFrame, title, true);
-    dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-
-    final TextComponentTags[] currentField = new TextComponentTags[] { null };
-    final HashMap<TextComponentTags, String> mapTextComponentTagsToFieldname =
-      new HashMap<TextComponentTags, String>();
-
-    Box headers = Box.createHorizontalBox();
-    final JButton insertFieldButton =
-      new JPotentiallyOverlongPopupMenuButton(L.m("Serienbrieffeld"),
-        new Iterable<Action>()
-        {
-          public Iterator<Action> iterator()
-          {
-            List<Action> actions = new Vector<Action>();
-            List<String> columnNames = ds.getColumnNames();
-
-            Collections.sort(columnNames);
-
-            Iterator<String> iter = columnNames.iterator();
-            while (iter.hasNext())
-            {
-              final String name = iter.next();
-              Action button = new AbstractAction(name)
-              {
-                private static final long serialVersionUID = 0;
-
-                public void actionPerformed(ActionEvent e)
-                {
-                  if (currentField[0] != null) currentField[0].insertTag(name);
-                }
-              };
-              actions.add(button);
-            }
-
-            return actions.iterator();
-          }
-        });
-    insertFieldButton.setFocusable(false);
-    headers.add(Box.createHorizontalGlue());
-    headers.add(insertFieldButton);
-
-    Box itemBox = Box.createVerticalBox();
-    ArrayList<JLabel> labels = new ArrayList<JLabel>();
-    int maxLabelWidth = 0;
-
-    Box hbox = Box.createHorizontalBox();
-    JLabel label = new JLabel(labelOldFields);
-    labels.add(label);
-    maxLabelWidth = DimAdjust.maxWidth(maxLabelWidth, label);
-    label.setBorder(BorderFactory.createEmptyBorder(5, 5, 10, 5));
-    hbox.add(label);
-    label = new JLabel(labelNewFields);
-    label.setBorder(BorderFactory.createEmptyBorder(5, 5, 10, 5));
-    DimAdjust.maxHeightIsPrefMaxWidthUnlimited(label);
-    hbox.add(label);
-    hbox.add(Box.createHorizontalStrut(200));
-    DimAdjust.maxHeightIsPrefMaxWidthUnlimited(hbox);
-    itemBox.add(hbox);
-
-    HashSet<String> addedFields = new HashSet<String>();
-    for (int i = 0; i < fieldIDs.length; i++)
-    {
-      String fieldId = fieldIDs[i].getFieldId();
-      if (addedFields.contains(fieldId)) continue;
-      final boolean isTransformed = fieldIDs[i].isTransformed();
-      addedFields.add(fieldId);
-
-      hbox = Box.createHorizontalBox();
-
-      label = new JLabel(fieldId);
-      label.setFont(label.getFont().deriveFont(Font.PLAIN));
-      labels.add(label);
-      maxLabelWidth = DimAdjust.maxWidth(maxLabelWidth, label);
-      label.setBorder(BorderFactory.createEmptyBorder(5, 5, 10, 5));
-      hbox.add(label);
-
-      final TextComponentTags field = new TextComponentTags(new JTextField())
-      {
-        public boolean isContentValid()
-        {
-          if (!isTransformed) return true;
-          List<TextComponentTags.ContentElement> c = getContent();
-          if (c.size() == 0) return true;
-          return c.size() == 1 && c.get(0).isTag();
-        }
-      };
-      mapTextComponentTagsToFieldname.put(field, fieldId);
-      Box fbox = Box.createHorizontalBox();
-      hbox.add(fbox); // fbox für zeilenbündige Ausrichtung benötigt
-      fbox.setBorder(BorderFactory.createEmptyBorder(5, 5, 10, 5));
-      field.getJTextComponent().addFocusListener(new FocusListener()
-      {
-        public void focusLost(FocusEvent e)
-        {}
-
-        public void focusGained(FocusEvent e)
-        {
-          currentField[0] = field;
-        }
-      });
-      DimAdjust.maxHeightIsPrefMaxWidthUnlimited(field.getJTextComponent());
-      fbox.add(field.getJTextComponent());
-
-      itemBox.add(hbox);
-    }
-
-    // einheitliche Breite für alle Labels vergeben:
-    for (Iterator<JLabel> iter = labels.iterator(); iter.hasNext();)
-    {
-      label = iter.next();
-
-      Dimension d = label.getPreferredSize();
-      d.width = maxLabelWidth + 10;
-      label.setPreferredSize(d);
-    }
-
-    Box buttonBox = Box.createHorizontalBox();
-    JButton button = new JButton(L.m("Abbrechen"));
-    button.addActionListener(new ActionListener()
-    {
-      public void actionPerformed(ActionEvent e)
-      {
-        dialog.dispose();
-      }
-    });
-    buttonBox.add(button);
-
-    buttonBox.add(Box.createHorizontalGlue());
-
-    button = new JButton(labelSubmitButton);
-    button.addActionListener(new ActionListener()
-    {
-      public void actionPerformed(ActionEvent e)
-      {
-        final HashMap<String, FieldSubstitution> result =
-          new HashMap<String, FieldSubstitution>();
-        for (Iterator<TextComponentTags> iter =
-          mapTextComponentTagsToFieldname.keySet().iterator(); iter.hasNext();)
-        {
-          TextComponentTags f = iter.next();
-          if (!f.isContentValid()) continue;
-          String fieldId = "" + mapTextComponentTagsToFieldname.get(f);
-          FieldSubstitution subst = new TextDocumentModel.FieldSubstitution();
-          for (TextComponentTags.ContentElement ce : f.getContent())
-          {
-            if (ce.isTag())
-              subst.addField(ce.toString());
-            else
-              subst.addFixedText(ce.toString());
-          }
-          result.put(fieldId, subst);
-        }
-
-        dialog.dispose();
-
-        if (submitActionListener != null) new Thread()
-        {
-          public void run()
-          {
-            submitActionListener.actionPerformed(new ActionEvent(result, 0,
-              "showSubstitutionDialogReturned"));
-          }
-        }.start();
-      }
-    });
-    buttonBox.add(button);
-
-    JScrollPane spane =
-      new JScrollPane(itemBox, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
-        ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-    spane.setBorder(BorderFactory.createEmptyBorder());
-    dialog.add(spane);
-
-    Box vbox = Box.createVerticalBox();
-    vbox.add(headers);
-    vbox.add(spane);
-    vbox.add(Box.createVerticalGlue());
-    vbox.add(buttonBox);
-    vbox.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-
-    dialog.add(vbox);
-
-    dialog.pack();
-    int frameWidth = dialog.getWidth();
-    int frameHeight = dialog.getHeight();
-    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    int x = screenSize.width / 2 - frameWidth / 2;
-    int y = screenSize.height / 2 - frameHeight / 2;
-    dialog.setLocation(x, y);
-    dialog.setVisible(true);
   }
 
   /**
