f88e7d812a72558432766295f1933dba6d058de7
bed <devnull@localhost>
2009-08-20 15:27:58 +0200

    [svn r3629] (BED) wollmuxbar.bat wurde abgelöst durch wollmuxbar.exe. Diese Datei muss sich beim Ausführen im selben Verzeichnis wie die WollMuxBar.jar befinden und sucht automatisch nach einer Java-Installation um die WollMuxBar zu starten. Ein manuelles Angeben des Java-Pfades wie in der wollmuxbar.bat ist daher nicht mehr nötig. Sollte dennoch Bedarf bestehen, kann die zu verwendende Java-Installation über den Wert "JavaHome" im Registrierungschlüssel HKCU\Software\WollMux (für einen Benutzer) bzw. HKLM\Software\WollMux (für alle Benutzer) festgelegt werden.

--- dist/wollmuxbar.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set "JAR_FILE=<Path to>\WollMuxBar.jar"
-                              
-set JAVA_HOME=C:\Programme\Java\<Java Version>
-set PATH=%JAVA_HOME%\bin;%PATH%
-
-start "WollMuxBar" javaw -jar "%JAR_FILE%" %1 %2 %3 %4 %5 %6 %7 %8
