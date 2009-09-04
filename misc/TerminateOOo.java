4f0d67b74d39524da4f752d2740cfb3af03c6e76
bnk <devnull@localhost>
2009-09-04 09:52:03 +0200

    [svn r3646] (BNK)Ins WollMux-Projekt verschoben

---
+++ misc/TerminateOOo.java
@@ -0,0 +1,101 @@
+/*
+ * Dateiname: TerminateOOo.java
+ * Projekt  : n/a
+ * Funktion : Kleines Tool zum Beenden von OpenOffice.org
+ * 
+ * Copyright (c) 2009 Landeshauptstadt München
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the European Union Public Licence (EUPL), 
+ * version 1.0.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * European Union Public Licence for more details.
+ *
+ * You should have received a copy of the European Union Public Licence
+ * along with this program. If not, see 
+ * http://ec.europa.eu/idabc/en/document/7330
+ *
+ * Änderungshistorie:
+ * Datum      | Wer | Änderungsgrund
+ * -------------------------------------------------------------------
+ * 03.09.2009 | BED | Erstellung
+ * -------------------------------------------------------------------
+ *
+ * @author Daniel Benkmann (D-III-ITD-D101)
+ * 
+ */
+package de.muenchen.allg.d101.test;
+
+import com.sun.star.beans.XPropertySet;
+import com.sun.star.comp.helper.Bootstrap;
+import com.sun.star.frame.XDesktop;
+import com.sun.star.lang.XMultiComponentFactory;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.uno.XComponentContext;
+
+/**
+ * Kleines Tool zum Beenden von OpenOffice.org.
+ * 
+ * @author Daniel Benkmann (D-III-ITD-D101)
+ */
+public class TerminateOOo
+{
+
+  /**
+   * Stellt eine Verbindung mit OpenOffice.org her (und startet dabei einen
+   * soffice-Prozess, falls noch keiner läuft) und beendet dann OpenOffice.org
+   * mittels XDesktop.terminate(). Das Programm beendet die JVM mit Statuscode 0,
+   * wenn das Beenden von Openoffice.org erfolgreich war und mit einem Statuscode !=
+   * 0, wenn OpenOffice.org nicht beendet wurde bzw. irgendetwas schief gelaufen ist
+   * (z.B. beim Verbindungsaufbau). Zudem wird auf der Standardausgabe eine
+   * entsprechende Meldung ausgegeben.
+   * 
+   * @param args
+   *          wird nicht ausgewertet
+   * @author Daniel Benkmann (D-III-ITD-D101)
+   */
+  public static void main(String[] args)
+  {
+    boolean terminated = false;
+    try
+    {
+      XMultiComponentFactory xMCF = Bootstrap.bootstrap().getServiceManager();
+      XComponentContext defaultContext =
+        (XComponentContext) UnoRuntime.queryInterface(
+          XComponentContext.class,
+          ((XPropertySet) UnoRuntime.queryInterface(XPropertySet.class, xMCF)).getPropertyValue("DefaultContext"));
+      XDesktop desktop =
+        (XDesktop) UnoRuntime.queryInterface(XDesktop.class,
+          xMCF.createInstanceWithContext("com.sun.star.frame.Desktop",
+            defaultContext));
+
+      // Quickstarter soll das Terminieren des Prozesses nicht verhindern
+      XPropertySet xPropSet =
+        (XPropertySet) UnoRuntime.queryInterface(XPropertySet.class, desktop);
+      xPropSet.setPropertyValue("SuspendQuickstartVeto", Boolean.TRUE);
+
+      // Versuch OOo zu beenden
+      terminated = desktop.terminate();
+    }
+    catch (Exception e)
+    {
+      System.out.println("Exception occured while trying to terminate OpenOffice.org!");
+      e.printStackTrace();
+      System.exit(2);
+    }
+
+    if (terminated)
+    {
+      System.out.println("OpenOffice.org was terminated successfully!");
+      System.exit(0);
+    }
+    else
+    {
+      System.out.println("OpenOffice.org was NOT terminated!");
+      System.exit(1);
+    }
+  }
+}
