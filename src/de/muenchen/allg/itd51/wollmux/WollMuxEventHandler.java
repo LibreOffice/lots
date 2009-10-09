f7ad501fc314d89b58aa146b148c6d5f63a70e95
bnk <devnull@localhost>
2009-10-09 17:00:04 +0200

    [svn r3748] (BNK)[R58676][#3684]Copyright und Autoreninfos in About-Box [R4504][#649]Hintergrundfarbe der Versionsinformationen sollte auch weiss sein

--- src/de/muenchen/allg/itd51/wollmux/WollMuxEventHandler.java
+++ src/de/muenchen/allg/itd51/wollmux/WollMuxEventHandler.java
@@ -40,7 +40,13 @@
  */
 package de.muenchen.allg.itd51.wollmux;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -65,9 +71,15 @@ import java.util.Vector;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.swing.AbstractAction;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JOptionPane;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import com.sun.star.awt.XWindow;
 import com.sun.star.beans.PropertyValue;
@@ -775,35 +787,144 @@ public class WollMuxEventHandler
     {
       WollMuxSingleton mux = WollMuxSingleton.getInstance();
 
-      String str = "WollMux " + mux.getBuildInfo();
-      if (wollMuxBarVersion != null && !wollMuxBarVersion.equals(""))
-        str += "\n" + L.m("WollMux-Leiste") + " " + wollMuxBarVersion;
-
-      str += "\n\n" + L.m("WollMux-Konfiguration:") + " " + mux.getConfVersionInfo();
-      str += "\n" + WollMuxFiles.getDEFAULT_CONTEXT().toExternalForm();
-
       Common.setLookAndFeelOnce();
 
-      ImageIcon icon = new ImageIcon(WM_URL);
+      // non-modal dialog. Set 3rd param to true to make modal
+      final JDialog dialog =
+        new JDialog((Frame) null, L.m("Info über Vorlagen und Formulare (WollMux)"),
+          false);
+      JPanel myPanel = new JPanel();
+      myPanel.setLayout(new BoxLayout(myPanel, BoxLayout.Y_AXIS));
+      myPanel.setBackground(Color.WHITE);
+      dialog.setBackground(Color.WHITE);
+      dialog.setContentPane(myPanel);
+      JPanel imagePanel = new JPanel(new BorderLayout());
+      imagePanel.add(new JLabel(new ImageIcon(WM_URL)), BorderLayout.CENTER);
+      imagePanel.setOpaque(false);
+      Box copyrightPanel = Box.createVerticalBox();
+      copyrightPanel.setOpaque(false);
+      Box hbox = Box.createHorizontalBox();
+      hbox.add(imagePanel);
+      hbox.add(copyrightPanel);
+      myPanel.add(hbox);
 
-      JOptionPane pane =
-        new JOptionPane(str, JOptionPane.INFORMATION_MESSAGE,
-          JOptionPane.DEFAULT_OPTION, icon);
-      JDialog dialog =
-        pane.createDialog(null, L.m("Info über Vorlagen und Formulare (WollMux)"));
+      copyrightPanel.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
+      JLabel label = new JLabel("WollMux " + mux.getVersion());
+      Font largeFont = label.getFont().deriveFont(15.0f);
+      label.setFont(largeFont);
+      copyrightPanel.add(label);
+      label = new JLabel("Copyright (c) 2005-2010 Landeshauptstadt München");
+      label.setFont(largeFont);
+      copyrightPanel.add(label);
+      label = new JLabel(L.m("Lizenz: %1", "European Union Public License"));
+      label.setFont(largeFont);
+      copyrightPanel.add(label);
+      label = new JLabel(L.m("Homepage: %1", "www.wollmux.org"));
+      label.setFont(largeFont);
+      copyrightPanel.add(label);
 
-      // Hintergrundfarbe aller Komponenten auf weiss setzen
-      try
+      Box authorOuterPanel = Box.createHorizontalBox();
+      authorOuterPanel.add(Box.createHorizontalStrut(8));
+
+      Box authorPanelHBox = Box.createHorizontalBox();
+      authorPanelHBox.setBorder(BorderFactory.createTitledBorder(L.m("Autoren (sortiert nach LOC)")));
+      authorOuterPanel.add(authorPanelHBox);
+      authorOuterPanel.add(Box.createHorizontalStrut(8));
+
+      Box authorPanel = Box.createVerticalBox();
+      authorPanelHBox.add(authorPanel);
+      authorPanel.setOpaque(false);
+      myPanel.add(authorOuterPanel);
+
+      int authorSpacing = 1;
+
+      hbox = Box.createHorizontalBox();
+      // hbox.add(new JLabel(new ImageIcon(MB_URL)));
+      // hbox.add(Box.createHorizontalStrut(8));
+      hbox.add(new JLabel("Matthias S. Benkmann"));
+      hbox.add(Box.createHorizontalGlue());
+      authorPanel.add(hbox);
+
+      hbox = Box.createHorizontalBox();
+      // hbox.add(new JLabel(new ImageIcon(CL_URL)));
+      // hbox.add(Box.createHorizontalStrut(8));
+      hbox.add(new JLabel("Christoph Lutz"));
+      hbox.add(Box.createHorizontalGlue());
+      authorPanel.add(Box.createVerticalStrut(authorSpacing));
+      authorPanel.add(hbox);
+
+      authorPanel.add(Box.createVerticalGlue());
+
+      // Box authorPanel2 = Box.createVerticalBox();
+      // authorPanelHBox.add(authorPanel2);
+      Box authorPanel2 = authorPanel;
+
+      hbox = Box.createHorizontalBox();
+      hbox.add(new JLabel("Daniel Benkmann"));
+      hbox.add(Box.createHorizontalGlue());
+      authorPanel2.add(Box.createVerticalStrut(authorSpacing));
+      authorPanel2.add(hbox);
+      hbox = Box.createHorizontalBox();
+      hbox.add(new JLabel("Bettina Bauer"));
+      hbox.add(Box.createHorizontalGlue());
+      authorPanel2.add(Box.createVerticalStrut(authorSpacing));
+      authorPanel2.add(hbox);
+      hbox = Box.createHorizontalBox();
+      hbox.add(new JLabel("Max Meier"));
+      hbox.add(Box.createHorizontalGlue());
+      authorPanel2.add(Box.createVerticalStrut(authorSpacing));
+      authorPanel2.add(hbox);
+
+      authorPanel2.add(Box.createVerticalGlue());
+
+      myPanel.add(Box.createVerticalStrut(8));
+      Box infoOuterPanel = Box.createHorizontalBox();
+      infoOuterPanel.add(Box.createHorizontalStrut(8));
+      JPanel infoPanel = new JPanel(new GridLayout(4, 1));
+      infoOuterPanel.add(infoPanel);
+      myPanel.add(infoOuterPanel);
+      infoOuterPanel.add(Box.createHorizontalStrut(8));
+      infoPanel.setOpaque(false);
+      infoPanel.setBorder(BorderFactory.createTitledBorder(L.m("Info")));
+
+      infoPanel.add(new JLabel("WollMux" + " " + mux.getBuildInfo()));
+
+      if (wollMuxBarVersion != null && !wollMuxBarVersion.equals(""))
+        infoPanel.add(new JLabel(L.m("WollMux-Leiste") + " " + wollMuxBarVersion));
+
+      infoPanel.add(new JLabel(L.m("WollMux-Konfiguration:") + " "
+        + mux.getConfVersionInfo()));
+
+      infoPanel.add(new JLabel("DEFAULT_CONTEXT: "
+        + WollMuxFiles.getDEFAULT_CONTEXT().toExternalForm()));
+
+      myPanel.add(Box.createVerticalStrut(4));
+
+      hbox = Box.createHorizontalBox();
+      hbox.add(Box.createHorizontalGlue());
+      hbox.add(new JButton(new AbstractAction(L.m("OK"))
       {
-        pane.setBackground(Color.WHITE);
-        pane.getComponent(0).setBackground(Color.WHITE);
-        pane.getComponent(1).setBackground(Color.WHITE);
-      }
-      catch (java.lang.Exception e)
-      {}
+        public void actionPerformed(ActionEvent e)
+        {
+          dialog.dispose();
+        }
+      }));
+      hbox.add(Box.createHorizontalGlue());
+      myPanel.add(hbox);
 
+      myPanel.add(Box.createVerticalStrut(4));
+
+      dialog.setBackground(Color.WHITE);
+      myPanel.setBackground(Color.WHITE);
+      dialog.pack();
+      int frameWidth = dialog.getWidth();
+      int frameHeight = dialog.getHeight();
+      Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+      int x = screenSize.width / 2 - frameWidth / 2;
+      int y = screenSize.height / 2 - frameHeight / 2;
+      dialog.setLocation(x, y);
       dialog.setAlwaysOnTop(true);
-      dialog.setVisible(true); // Wartet bis OK Dialog beendet!
+      dialog.setVisible(true);
     }
 
     public String toString()
