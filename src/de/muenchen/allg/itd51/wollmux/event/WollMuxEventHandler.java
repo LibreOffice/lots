bcf1f5c783046b838d793d094ca27f0a0028fce6
Patric Busanny <patric.busanny@muenchen.de>
2016-01-13 09:00:32 +0100

    Update des Copyright-Vermerks in allen .java-Headern: jetzt "Copyright (c) 200x-2016 Landeshauptstadt München"

--- src/de/muenchen/allg/itd51/wollmux/event/WollMuxEventHandler.java
+++ src/de/muenchen/allg/itd51/wollmux/event/WollMuxEventHandler.java
@@ -796,7 +796,7 @@ public class WollMuxEventHandler
       Font largeFont = label.getFont().deriveFont(15.0f);
       label.setFont(largeFont);
       copyrightPanel.add(label);
-      label = new JLabel(L.m("Copyright (c) 2005-2011 Landeshauptstadt München"));
+      label = new JLabel(L.m("Copyright (c) 2005-2016 Landeshauptstadt München"));
       label.setFont(largeFont);
       copyrightPanel.add(label);
       label = new JLabel(L.m("Lizenz: %1", "European Union Public License"));
