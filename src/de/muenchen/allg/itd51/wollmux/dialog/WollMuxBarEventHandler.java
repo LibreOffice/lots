26936cb61e9fdc9e48e6f91ea004737d10f8a091
Daniel Sikeler <daniel.sikeler@muenchen.de>
2016-11-15 14:34:00 +0100

    trac#19055 Starte Office mit den gleichen Argumenten
    Office wird nicht mehr mit den Argumenten --nolockcheck und --norestore gestartet

--- src/de/muenchen/allg/itd51/wollmux/dialog/WollMuxBarEventHandler.java
+++ src/de/muenchen/allg/itd51/wollmux/dialog/WollMuxBarEventHandler.java
@@ -533,7 +533,17 @@ public class WollMuxBarEventHandler
       XComponentContext ctx = null;
       try
       {
-        ctx = Bootstrap.bootstrap();
+        try
+        {
+          String[] args = {"--nologo", "--nodefault", "--minimized"};
+          Bootstrap.class.getMethod("bootstrap", args.getClass());
+          ctx = Bootstrap.bootstrap(args);
+        }
+        // Verwende default Bootstrap-Methode. Diese startet Office mit anderen Parametern.
+        catch (Exception e)
+        {
+          ctx = Bootstrap.bootstrap();
+        }
         factory =
           UnoRuntime.queryInterface(
             XMultiServiceFactory.class, ctx.getServiceManager());
