39b2317b88d0b51445b7e78c2ba0e117daae49c5
Nghiem Tran <nghiem_tran5@yahoo.com>
2018-11-23 11:32:01 +0100

    Abfrage des Flags acceptEvents

--- src/de/muenchen/allg/itd51/wollmux/event/WollMuxEventListener.java
+++ src/de/muenchen/allg/itd51/wollmux/event/WollMuxEventListener.java
@@ -1,6 +1,5 @@
 package de.muenchen.allg.itd51.wollmux.event;
 
-import com.google.common.eventbus.AllowConcurrentEvents;
 import com.google.common.eventbus.Subscribe;
 
 import de.muenchen.allg.itd51.wollmux.event.handlers.OnAbdruck;
