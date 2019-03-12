45e35bf4011d9c1f2b3ad8cae7342b39af3b20a1
bjoernranft <externer.dl.ranft@muenchen.de>
2019-03-13 00:49:33 +0100

    OnAbout UNO-Dialog.

--- src/de/muenchen/allg/itd51/wollmux/event/handlers/OnAbout.java
+++ src/de/muenchen/allg/itd51/wollmux/event/handlers/OnAbout.java
@@ -1,30 +1,30 @@
 package de.muenchen.allg.itd51.wollmux.event.handlers;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Frame;
-import java.awt.GridLayout;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
-import javax.swing.AbstractAction;
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.sun.star.awt.XWindow;
+import com.sun.star.beans.PropertyVetoException;
+import com.sun.star.beans.UnknownPropertyException;
+import com.sun.star.beans.XPropertySet;
+import com.sun.star.lang.IllegalArgumentException;
+import com.sun.star.lang.WrappedTargetException;
+
+import de.muenchen.allg.afid.UNO;
 import de.muenchen.allg.itd51.wollmux.WollMuxFehlerException;
-import de.muenchen.allg.itd51.wollmux.WollMuxFiles;
 import de.muenchen.allg.itd51.wollmux.WollMuxSingleton;
-import de.muenchen.allg.itd51.wollmux.core.util.L;
-import de.muenchen.allg.itd51.wollmux.dialog.Common;
+import de.muenchen.allg.itd51.wollmux.core.dialog.ControlModel;
+import de.muenchen.allg.itd51.wollmux.core.dialog.ControlModel.Align;
+import de.muenchen.allg.itd51.wollmux.core.dialog.ControlModel.ControlType;
+import de.muenchen.allg.itd51.wollmux.core.dialog.ControlModel.Orientation;
+import de.muenchen.allg.itd51.wollmux.core.dialog.ControlProperties;
+import de.muenchen.allg.itd51.wollmux.core.dialog.SimpleDialogLayout;
+import de.muenchen.allg.itd51.wollmux.core.dialog.UNODialogFactory;
 
 /**
  * Erzeugt ein neues WollMuxEvent, das einen modalen Dialog anzeigt, der wichtige
@@ -40,10 +40,14 @@ import de.muenchen.allg.itd51.wollmux.dialog.Common;
  */
 public class OnAbout extends BasicEvent
 {
+  private static final Logger LOGGER = LoggerFactory
+      .getLogger(OnAbout.class);
   private String wollMuxBarVersion;
+  private UNODialogFactory dialogFactory;
+  private SimpleDialogLayout layout;
 
   private final URL WM_URL = this.getClass().getClassLoader().getResource(
-      "data/wollmux_klein.jpg");
+      "data/wollmux.jpg");
 
   public OnAbout(String wollMuxBarVersion)
   {
@@ -53,150 +57,127 @@ public class OnAbout extends BasicEvent
   @Override
   protected void doit() throws WollMuxFehlerException
   {
-    Common.setLookAndFeelOnce();
+    
+    dialogFactory = new UNODialogFactory();
+    XWindow dialogWindow = dialogFactory.createDialog(780, 450, 0xF2F2F2);
 
-    // non-modal dialog. Set 3rd param to true to make modal
-    final JDialog dialog = new JDialog((Frame) null,
-        L.m("Info über Vorlagen und Formulare (WollMux)"),
-        false);
-    JPanel myPanel = new JPanel();
-    myPanel.setLayout(new BoxLayout(myPanel, BoxLayout.Y_AXIS));
-    myPanel.setBackground(Color.WHITE);
-    dialog.setBackground(Color.WHITE);
-    dialog.setContentPane(myPanel);
-    JPanel imagePanel = new JPanel(new BorderLayout());
-    imagePanel.add(new JLabel(new ImageIcon(WM_URL)), BorderLayout.CENTER);
-    imagePanel.setOpaque(false);
-    Box copyrightPanel = Box.createVerticalBox();
-    copyrightPanel.setOpaque(false);
-    Box hbox = Box.createHorizontalBox();
-    hbox.add(imagePanel);
-    hbox.add(copyrightPanel);
-    myPanel.add(hbox);
+    dialogFactory.showDialog();
 
-    copyrightPanel.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
-    JLabel label = new JLabel(
-        L.m("WollMux") + " " + WollMuxSingleton.getVersion());
-    Font largeFont = label.getFont().deriveFont(15.0f);
-    label.setFont(largeFont);
-    copyrightPanel.add(label);
-    label = new JLabel(L.m("Copyright (c) 2005-2018 Landeshauptstadt München"));
-    label.setFont(largeFont);
-    copyrightPanel.add(label);
-    label = new JLabel(L.m("Lizenz: %1", "European Union Public License"));
-    label.setFont(largeFont);
-    copyrightPanel.add(label);
-    label = new JLabel(L.m("Homepage: %1", "www.wollmux.org"));
-    label.setFont(largeFont);
-    copyrightPanel.add(label);
+    layout = new SimpleDialogLayout(dialogWindow);
+    layout.setMarginBetweenControls(15);
+    layout.setMarginTop(20);
+    layout.setMarginLeft(20);
+    layout.setWindowBottomMargin(10);
 
-    Box authorOuterPanel = Box.createHorizontalBox();
-    authorOuterPanel.add(Box.createHorizontalStrut(8));
+    layout.addControlsToList(addIntroControls());
+    layout.addControlsToList(addAuthors());
+    layout.addControlsToList(addInfo());
 
-    Box authorPanelHBox = Box.createHorizontalBox();
-    authorPanelHBox.setBorder(
-        BorderFactory.createTitledBorder(L.m("Autoren (sortiert nach LOC)")));
-    authorOuterPanel.add(authorPanelHBox);
-    authorOuterPanel.add(Box.createHorizontalStrut(8));
+    layout.draw();
+  }
+  
+  private ControlModel addInfo()
+  {
+    List<ControlProperties> introControls = new ArrayList<>();
+    
+    ControlProperties info = new ControlProperties(ControlType.LABEL, "info");
+    info.setControlPercentSize(100, 20);
+    info.setLabel("Info");
+    
+    ControlProperties hLine = new ControlProperties(ControlType.LINE, "hLine2");
+    hLine.setControlPercentSize(100, 10);
+    
+    ControlProperties wmVersion = new ControlProperties(ControlType.LABEL, "wmVersion");
+    wmVersion.setControlPercentSize(100, 20);
+    wmVersion.setLabel("WollMux Version: " + WollMuxSingleton.getVersion());
+    
+    ControlProperties wmBar = new ControlProperties(ControlType.LABEL, "wmBar");
+    wmBar.setControlPercentSize(100, 20);
+    wmBar.setLabel("WollMux-Leiste Version: " + wollMuxBarVersion);
+    
+    ControlProperties wmConfig = new ControlProperties(ControlType.LABEL, "wmConfig");
+    wmConfig.setControlPercentSize(100, 20);
+    wmConfig.setLabel("WollMux-Konfiguration: " + WollMuxSingleton.getInstance().getConfVersionInfo());
+    
+    introControls.add(info);
+    introControls.add(hLine);
+    introControls.add(wmVersion);
+    introControls.add(wmBar);
+    introControls.add(wmConfig);
+    
+    return new ControlModel(Orientation.VERTICAL, Align.NONE, introControls, Optional.empty());
+  }
+  
+  private ControlModel addAuthors()
+  {
+    List<ControlProperties> introControls = new ArrayList<>();
 
-    Box authorPanel = Box.createVerticalBox();
-    authorPanelHBox.add(authorPanel);
-    authorPanel.setOpaque(false);
-    myPanel.add(authorOuterPanel);
+    ControlProperties autoren = new ControlProperties(ControlType.LABEL, "labelAutors");
+    autoren.setControlPercentSize(100, 20);
+    autoren.setLabel("Autoren");
+    
+    ControlProperties hLine = new ControlProperties(ControlType.LINE, "hLine");
+    hLine.setControlPercentSize(100, 10);
+    
+    ControlProperties bk = new ControlProperties(ControlType.LABEL, "bk");
+    bk.setControlPercentSize(100, 20);
+    bk.setLabel("Matthias S. Benkmann");
+    
+    ControlProperties cl = new ControlProperties(ControlType.LABEL, "cl");
+    cl.setControlPercentSize(100, 20);
+    cl.setLabel("Christoph Lutz");
+    
+    ControlProperties bk1 = new ControlProperties(ControlType.LABEL, "bk1");
+    bk1.setControlPercentSize(100, 20);
+    bk1.setLabel("Daniel Benkmann");
+    
+    ControlProperties bb = new ControlProperties(ControlType.LABEL, "bb");
+    bb.setControlPercentSize(100, 20);
+    bb.setLabel("Bettina Bauer");
+    
+    ControlProperties ae = new ControlProperties(ControlType.LABEL, "ae");
+    ae.setControlPercentSize(100, 20);
+    ae.setLabel("Andor Ertsey");
+    
+    ControlProperties mm = new ControlProperties(ControlType.LABEL, "mm");
+    mm.setControlPercentSize(100, 20);
+    mm.setLabel("Max Meier");
+    
+    introControls.add(autoren);
+    introControls.add(hLine);
+    introControls.add(bk);
+    introControls.add(cl);
+    introControls.add(bk1);
+    introControls.add(ae);
+    introControls.add(mm);
+    
+    return new ControlModel(Orientation.VERTICAL, Align.NONE, introControls, Optional.empty());
+  }
+  
+  private ControlModel addIntroControls()
+  {
+    List<ControlProperties> introControls = new ArrayList<>();
 
-    int authorSpacing = 1;
+    ControlProperties image = new ControlProperties(ControlType.IMAGE_CONTROL, "wmImage");
+    image.setControlPercentSize(30, 100);
 
-    hbox = Box.createHorizontalBox();
-    hbox.add(new JLabel("Matthias S. Benkmann"));
-    hbox.add(Box.createHorizontalGlue());
-    authorPanel.add(hbox);
-
-    hbox = Box.createHorizontalBox();
-    hbox.add(new JLabel("Christoph Lutz"));
-    hbox.add(Box.createHorizontalGlue());
-    authorPanel.add(Box.createVerticalStrut(authorSpacing));
-    authorPanel.add(hbox);
-
-    authorPanel.add(Box.createVerticalGlue());
-
-    Box authorPanel2 = authorPanel;
-
-    hbox = Box.createHorizontalBox();
-    hbox.add(new JLabel("Daniel Benkmann"));
-    hbox.add(Box.createHorizontalGlue());
-    authorPanel2.add(Box.createVerticalStrut(authorSpacing));
-    authorPanel2.add(hbox);
-    hbox = Box.createHorizontalBox();
-    hbox.add(new JLabel("Bettina Bauer"));
-    hbox.add(Box.createHorizontalGlue());
-    authorPanel2.add(Box.createVerticalStrut(authorSpacing));
-    authorPanel2.add(hbox);
-    hbox = Box.createHorizontalBox();
-    hbox.add(new JLabel("Andor Ertsey"));
-    hbox.add(Box.createHorizontalGlue());
-    authorPanel2.add(Box.createVerticalStrut(authorSpacing));
-    authorPanel2.add(hbox);
-    hbox = Box.createHorizontalBox();
-    hbox.add(new JLabel("Max Meier"));
-    hbox.add(Box.createHorizontalGlue());
-    authorPanel2.add(Box.createVerticalStrut(authorSpacing));
-    authorPanel2.add(hbox);
-
-    authorPanel2.add(Box.createVerticalGlue());
-
-    myPanel.add(Box.createVerticalStrut(8));
-    Box infoOuterPanel = Box.createHorizontalBox();
-    infoOuterPanel.add(Box.createHorizontalStrut(8));
-    JPanel infoPanel = new JPanel(new GridLayout(4, 1));
-    infoOuterPanel.add(infoPanel);
-    myPanel.add(infoOuterPanel);
-    infoOuterPanel.add(Box.createHorizontalStrut(8));
-    infoPanel.setOpaque(false);
-    infoPanel.setBorder(BorderFactory.createTitledBorder(L.m("Info")));
-
-    infoPanel.add(
-        new JLabel(L.m("WollMux") + " " + WollMuxSingleton.getBuildInfo()));
-
-    if (wollMuxBarVersion != null && !wollMuxBarVersion.equals(""))
-      infoPanel
-          .add(new JLabel(L.m("WollMux-Leiste") + " " + wollMuxBarVersion));
-
-    infoPanel.add(new JLabel(L.m("WollMux-Konfiguration:") + " "
-        + WollMuxSingleton.getInstance().getConfVersionInfo()));
-
-    infoPanel.add(new JLabel("DEFAULT_CONTEXT: "
-        + WollMuxFiles.getDEFAULT_CONTEXT().toExternalForm()));
-
-    myPanel.add(Box.createVerticalStrut(4));
-
-    hbox = Box.createHorizontalBox();
-    hbox.add(Box.createHorizontalGlue());
-    hbox.add(new JButton(new AbstractAction(L.m("OK"))
+    XPropertySet props = UNO.XPropertySet(image.getXControl().getModel());
+    try
     {
-      private static final long serialVersionUID = 4527702807001201116L;
-
-      @Override
-      public void actionPerformed(ActionEvent e)
-      {
-        dialog.dispose();
-      }
-    }));
-    hbox.add(Box.createHorizontalGlue());
-    myPanel.add(hbox);
-
-    myPanel.add(Box.createVerticalStrut(4));
-
-    dialog.setBackground(Color.WHITE);
-    myPanel.setBackground(Color.WHITE);
-    dialog.pack();
-    int frameWidth = dialog.getWidth();
-    int frameHeight = dialog.getHeight();
-    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    int x = screenSize.width / 2 - frameWidth / 2;
-    int y = screenSize.height / 2 - frameHeight / 2;
-    dialog.setLocation(x, y);
-    dialog.setAlwaysOnTop(true);
-    dialog.setVisible(true);
+      props.setPropertyValue("ImageURL", WM_URL.toString());
+    } catch (IllegalArgumentException | UnknownPropertyException | PropertyVetoException | WrappedTargetException e)
+    {
+      LOGGER.error("", e);
+    } 
+    
+    ControlProperties label = new ControlProperties(ControlType.LABEL, "introLabel");
+    label.setControlPercentSize(70, 20);
+    label.setLabel("WollMux " + WollMuxSingleton.getVersion());
+    
+    introControls.add(image);
+    introControls.add(label);
+    
+    return new ControlModel(Orientation.HORIZONTAL, Align.NONE, introControls, Optional.empty());
   }
 
   @Override
