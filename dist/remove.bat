883cbb3c39c271b2d850a9fceb957139962a303b
Daniel Sikeler <daniel.sikeler@muenchen.de>
2019-12-06 13:19:27 +0100

    remove java-installer
    The java installer was originally used for installing WollMux at Windows
    hosts. It is better to unpack the oxt into the extension folder of
    LibreOffice.
    
    For private installations the oxt-file can be installed by double-clicking.

--- dist/remove.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-"${oooPath}\unopkg" remove WollMux.uno.pkg
-"${oooPath}\unopkg" remove WollMux.uno.pkg --shared
-"${oooPath}\unopkg" remove de.muenchen.allg.d101.wollmux
-"${oooPath}\unopkg" remove de.muenchen.allg.d101.wollmux --shared
-exit 0
--- dist/remove.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-unopkg remove WollMux.uno.pkg
-unopkg remove WollMux.uno.pkg --shared
-unopkg remove de.muenchen.allg.d101.wollmux
-unopkg remove de.muenchen.allg.d101.wollmux --shared
-exit 0
