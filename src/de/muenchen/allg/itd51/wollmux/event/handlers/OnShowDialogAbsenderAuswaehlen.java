b9998d2dc550d4d257e3626515b768b048de3ad7
Daniel Sikeler <daniel.sikeler@muenchen.de>
2019-12-17 14:14:52 +0100

    configuration of pal is never used

--- src/de/muenchen/allg/itd51/wollmux/event/handlers/OnShowDialogAbsenderAuswaehlen.java
+++ src/de/muenchen/allg/itd51/wollmux/event/handlers/OnShowDialogAbsenderAuswaehlen.java
@@ -1,11 +1,8 @@
 package de.muenchen.allg.itd51.wollmux.event.handlers;
 
 import de.muenchen.allg.itd51.wollmux.WollMuxFehlerException;
-import de.muenchen.allg.itd51.wollmux.WollMuxFiles;
-import de.muenchen.allg.itd51.wollmux.core.parser.ConfigThingy;
 import de.muenchen.allg.itd51.wollmux.db.DatasourceJoinerFactory;
 import de.muenchen.allg.itd51.wollmux.dialog.AbsenderAuswaehlen;
-import de.muenchen.allg.itd51.wollmux.event.WollMuxEventHandler;
 
 /**
  * Dieses Event wird vom WollMux-Service (...comp.WollMux) und aus dem
@@ -19,19 +16,6 @@ public class OnShowDialogAbsenderAuswaehlen extends BasicEvent
   @Override
   protected void doit() throws WollMuxFehlerException
   {
-    ConfigThingy conf = WollMuxFiles.getWollmuxConf();
-
-    try
-    {
-      // Konfiguration auslesen:
-      ConfigThingy palConf = WollMuxEventHandler.getInstance()
-          .requireLastSection(conf, "PersoenlicheAbsenderliste");
-
-      new AbsenderAuswaehlen(palConf,
-          DatasourceJoinerFactory.getDatasourceJoiner());
-    } catch (Exception e)
-    {
-      throw new CantStartDialogException(e);
-    }
+    new AbsenderAuswaehlen(DatasourceJoinerFactory.getDatasourceJoiner());
   }
 }
