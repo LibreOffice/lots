0dd42b3f36b143795abfb6d120128fdf944362b8
Daniel Sikeler <daniel.sikeler@muenchen.de>
2020-03-11 14:44:18 +0100

    register components via services

---
+++ src/main/java/de/muenchen/allg/itd51/wollmux/sidebar/WollMuxBarRegistration.java
@@ -0,0 +1,29 @@
+package de.muenchen.allg.itd51.wollmux.sidebar;
+
+import de.muenchen.allg.itd51.wollmux.ComponentRegistration;
+
+/**
+ * Factory for {@link WollMuxSidebarFactory}.
+ */
+public class WollMuxBarRegistration implements ComponentRegistration
+{
+
+  @Override
+  public Class<?> getComponent()
+  {
+    return WollMuxSidebarFactory.class;
+  }
+
+  @Override
+  public String getName()
+  {
+    return WollMuxSidebarFactory.class.getName();
+  }
+
+  @Override
+  public String[] getServiceNames()
+  {
+    return new String[] { WollMuxSidebarFactory.__serviceName };
+  }
+
+}
