11d90866f8491e574c0ba375d98ef3533131630f
Daniel Sikeler <daniel.sikeler@muenchen.de>
2020-06-10 08:28:02 +0200

    remove unused code from FormModelException

--- src/main/java/de/muenchen/allg/itd51/wollmux/form/model/FormModelException.java
+++ src/main/java/de/muenchen/allg/itd51/wollmux/form/model/FormModelException.java
@@ -1,40 +1,35 @@
 package de.muenchen.allg.itd51.wollmux.form.model;
 
 /**
- * Ein Fehler in der Konfiguration des Formulars.
- * 
- * @author daniel.sikeler
- *
+ * An invalid form model.
  */
 public class FormModelException extends Exception
 {
 
   private static final long serialVersionUID = 5470806103796601018L;
 
-  public FormModelException()
-  {
-    super();
-  }
-
-  public FormModelException(String message, Throwable cause, boolean enableSuppression,
-      boolean writableStackTrace)
-  {
-    super(message, cause, enableSuppression, writableStackTrace);
-  }
-
+  /**
+   * An invalid form model.
+   *
+   * @param message
+   *          The description of the error.
+   * @param cause
+   *          The causing exception.
+   */
   public FormModelException(String message, Throwable cause)
   {
     super(message, cause);
   }
 
+  /**
+   * An invalid form model.
+   *
+   * @param message
+   *          The description of the error.
+   */
   public FormModelException(String message)
   {
     super(message);
-  }
-
-  public FormModelException(Throwable cause)
-  {
-    super(cause);
   }
 
 }
