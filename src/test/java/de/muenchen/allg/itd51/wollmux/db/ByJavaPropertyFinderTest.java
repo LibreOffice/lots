5fd44d55fece9ed441def8b82cefb33b9e64f485
Daniel Sikeler <daniel.sikeler@muenchen.de>
2020-07-21 08:20:31 +0200

    add DataFinder tests

---
+++ src/test/java/de/muenchen/allg/itd51/wollmux/db/ByJavaPropertyFinderTest.java
@@ -0,0 +1,42 @@
+/*-
+ * #%L
+ * WollMux
+ * %%
+ * Copyright (C) 2005 - 2020 Landeshauptstadt München
+ * %%
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * http://ec.europa.eu/idabc/eupl5
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ * #L%
+ */
+package de.muenchen.allg.itd51.wollmux.db;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ByJavaPropertyFinderTest
+{
+
+  @Test
+  void testJavaPropertyFinder()
+  {
+    DataFinder dataFinder = new ByJavaPropertyFinder(null);
+    assertFalse(dataFinder.getValueForKey("java.home").isEmpty());
+    assertNull(dataFinder.getValueForKey("unknown"));
+    assertTrue(dataFinder.getValueForKey(null).isEmpty());
+  }
+}
