c8ae7bc187d4134f0fc1e688ae3f199720d74842
Daniel Sikeler <daniel.sikeler@muenchen.de>
2020-10-23 13:27:44 +0200

    refactor DatasetPredicateTest

--- core/src/test/java/de/muenchen/allg/itd51/wollmux/db/DatasetPredicateTest.java
+++ core/src/test/java/de/muenchen/allg/itd51/wollmux/db/DatasetPredicateTest.java
@@ -26,8 +26,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
@@ -39,41 +37,21 @@ public class DatasetPredicateTest
   @Test
   public void makePredicateValidEntry() throws Exception
   {
-    List<QueryPart> query = new ArrayList<>();
-    query.add(new QueryPart("Vorname", "*Sheldon"));
-    query.add(new QueryPart("Vorname", "Sheldon")); // Valid
-    query.add(new QueryPart("Vorname", "Sheldon*"));
-
+    List<QueryPart> query = List.of(
+        new QueryPart("Vorname", "*Sheldon"), new QueryPart("Vorname", "Sheldon"), // Valid
+        new QueryPart("Vorname", "Sheldon*"));
     Predicate<Dataset> pred = DatasetPredicate.makePredicate(query);
-
-    Map<String, String> testMap = new HashMap<>();
-    testMap.put("Vorname", "Sheldon");
-
-    Dataset ds = new SimpleDataset("Test", testMap);
-
-    boolean result = pred.test(ds);
-
-    assertTrue(result, "Given dataset does not match with a given QueryPart:");
+    Dataset ds = new SimpleDataset("Test", Map.of("Vorname", "Sheldon"));
+    assertTrue(pred.test(ds), "Given dataset does not match with a given QueryPart:");
   }
 
   @Test
   public void makePredicateInValidEntry() throws Exception
   {
-    List<QueryPart> query = new ArrayList<>();
-
-    query.add(new QueryPart("Vorname", "*Sheldon"));
-    query.add(new QueryPart("Vorname", "Sheldon*"));
-
+    List<QueryPart> query = List.of(new QueryPart("Vorname", "*Sheldon"), new QueryPart("Vorname", "Sheldon*"));
     Predicate<Dataset> pred = DatasetPredicate.makePredicate(query);
-
-    Map<String, String> testMap = new HashMap<>();
-    testMap.put("Vorname", "Sheldon");
-
-    Dataset ds = new SimpleDataset("Test", testMap);
-
-    boolean result = pred.test(ds);
-
-    assertTrue(result, "Given dataset does not match with a given QueryPart:");
+    Dataset ds = new SimpleDataset("Test", Map.of("Vorname", "Sheldon"));
+    assertTrue(pred.test(ds), "Given dataset does not match with a given QueryPart:");
   }
 
   @Test
@@ -93,97 +71,36 @@ public class DatasetPredicateTest
   @Test
   public void makePredicateCheckCount() throws Exception
   {
-    List<QueryPart> query = new ArrayList<>();
-
-    query.add(new QueryPart("Vorname", "*Sheldon"));
-    query.add(new QueryPart("Vorname", "Sheldon*"));
-    query.add(new QueryPart("Vorname", "*Sheldon*"));
-    query.add(new QueryPart("Vorname", "*Sheldon**"));
-    query.add(new QueryPart("Vorname", "**Sheldon*"));
-    query.add(new QueryPart("Vorname", "**Sheldon**"));
-
+    List<QueryPart> query = List.of(new QueryPart("Vorname", "*Sheldon"), new QueryPart("Vorname", "Sheldon*"),
+        new QueryPart("Vorname", "*Sheldon*"), new QueryPart("Vorname", "*Sheldon**"),
+        new QueryPart("Vorname", "**Sheldon*"), new QueryPart("Vorname", "**Sheldon**"));
     Predicate<Dataset> pred = DatasetPredicate.makePredicate(query);
 
-    Map<String, String> testMap = new HashMap<>();
-    testMap.put("Vorname", "Sheldon");
+    List<Dataset> datasetList = List.of(new SimpleDataset("Test", Map.of("Vorname", "Sheldon")),
+        new SimpleDataset("Test", Map.of("Vorname", "Lennard")));
 
-    Map<String, String> testMap1 = new HashMap<>();
-    testMap1.put("Vorname", "Lennard");
-
-    List<Dataset> datasetList = new ArrayList<>();
-
-    Dataset ds = new SimpleDataset("Test", testMap);
-    Dataset ds1 = new SimpleDataset("Test", testMap1);
-
-    datasetList.add(ds);
-    datasetList.add(ds1);
-
-    int count = 0;
-    for (Dataset dataset : datasetList)
-    {
-      boolean result = pred.test(dataset);
-
-      if (result)
-        count++;
-    }
-
-    assertEquals(1, count, "Given datasets have a wrong match count with given QueryParts:");
+    assertEquals(1, datasetList.stream().filter(pred::test).count(),
+        "Given datasets have a wrong match count with given QueryParts:");
   }
 
   @Test
   public void unknwonColumn()
   {
-    List<QueryPart> query = new ArrayList<>();
-
-    query.add(new QueryPart("Vorname", "*Sheldon"));
-    query.add(new QueryPart("Vorname", "Sheldon*"));
-
+    List<QueryPart> query = List.of(new QueryPart("Vorname", "*Sheldon"), new QueryPart("Vorname", "Sheldon*"));
     Predicate<Dataset> pred = DatasetPredicate.makePredicate(query);
-
-    Map<String, String> testMap = new HashMap<>();
-    testMap.put("Nachname", "Couper");
-
-    Dataset ds = new SimpleDataset("Test", testMap);
-
-    boolean result = pred.test(ds);
-
-    assertFalse(result, "Given dataset does not match with a given QueryPart:");
+    Dataset ds = new SimpleDataset("Test", Map.of("Nachname", "Couper"));
+    assertFalse(pred.test(ds), "Given dataset does not match with a given QueryPart:");
   }
 
   @Test
   public void matchAll()
   {
     Predicate<Dataset> pred = DatasetPredicate.matchAll;
-
-    Map<String, String> testMap = new HashMap<>();
-    testMap.put("Vorname", "Sheldon");
-
-    Map<String, String> testMap1 = new HashMap<>();
-    testMap1.put("Vorname", "Sheldon*");
-
-    Map<String, String> testMap2 = new HashMap<>();
-    testMap1.put("Vorname", "**Sheldon**");
-
-    List<Dataset> datasetList = new ArrayList<>();
-
-    Dataset ds = new SimpleDataset("Test", testMap);
-    Dataset ds1 = new SimpleDataset("Test", testMap1);
-    Dataset ds2 = new SimpleDataset("Test", testMap2);
-
-    datasetList.add(ds);
-    datasetList.add(ds1);
-    datasetList.add(ds2);
-
-    int count = 0;
-    for (Dataset dataset : datasetList)
-    {
-      boolean result = pred.test(dataset);
-
-      if (result)
-        count++;
-    }
-
-    assertEquals(3, count, "Given datasets have a wrong match count with given QueryParts:");
+    List<Dataset> datasetList = List.of(new SimpleDataset("Test", Map.of("Vorname", "Sheldon")),
+        new SimpleDataset("Test", Map.of("Vorname", "Sheldon*")),
+        new SimpleDataset("Test", Map.of("Vorname", "**Sheldon**")));
+    assertEquals(3, datasetList.stream().filter(pred::test).count(),
+        "Given datasets have a wrong match count with given QueryParts:");
   }
 
 }
